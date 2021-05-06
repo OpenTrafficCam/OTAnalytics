314db5028d3aa16bd714aa3db3e28509bc8a6501
hendrik goerner <hendrikgoerner@gmx.de>
2021-05-06 13:03:36 +0200

    videocapture implemented

--- OTAnalytics/gui.py
+++ OTAnalytics/gui.py
@@ -1,111 +1,77 @@
-import tkinter as tk
-from tkinter import filedialog
-import PIL.Image, PIL.ImageTk
-from count import counting
-
-
-class Statepanel:
-    # class to give out states from current video and countings
-    def __init__(self, window, row, column, sticky):
-        self.scrollbar = tk.Scrollbar(window)
-        self.text = tk.Text(
-            window,
-            height=4,
-            width=100,
-            state="disabled",
-            yscrollcommand=self.scrollbar.set,
-            bg="white",
-        )
-        self.scrollbar.config(command=self.text.yview)
-        self.scrollbar.grid(
-            row=row, column=column, columnspan=2, padx="5", pady="3", sticky="e"
-        )
-        self.text.grid(row=row, column=column, padx="5", pady="3", sticky=sticky)
-
-        # new information
-
-    def update(self, text):
-        self.text.config(state="normal")
-        self.text.insert(tk.END, "- " + str(text) + "\n")
-        self.text.see("end")
-        self.text.config(state="disabled")
-
-
-class Controllbar:
-    def __init__(self, window, padx, side):
-        self.button1 = tk.Button(window, text="back")
-        self.button2 = tk.Button(window, text="play")
-        self.button3 = tk.Button(window, text="forward")
-        self.button4 = tk.Button(window, text="QUIT", fg="red", command=window.quit)
-
-        self.button1.pack(padx=padx, side=side)
-        self.button2.pack(padx=padx, side=side)
-        self.button3.pack(padx=padx, side=side)
-        self.button4.pack(padx=padx, side=side)
-
-
-class Sidepanel:
-    def __init__(self, window, padx, side, anchor):
-        self.button5 = tk.Button(
-            window,
-            text="load",
-            command=lambda: counting.video_count_window(),
-        )
-        self.button6 = tk.Button(window, text="save")
-
-        self.button6.pack(padx=padx, side=side, anchor=anchor)
-        self.button5.pack(padx=padx, side=side, anchor=anchor)
-
-
-class Filepath:
-    def __init__(self, window, folder_path_bool, text, width, text_button):
-        self.folder_path_bool = folder_path_bool
-        self.label = tk.Label(window, text=text)
-        self.entry = tk.Entry(window, width=width)
-
-        # search for the video file in the explorer to get the file path
-
-    def mtc_get_video_path(self, window):
-        if self.folder_path_bool:
-            new_filepath = filedialog.askdirectory(parent=window)
-        else:
-            new_filepath = filedialog.askopenfilename(parent=window)
-        print(new_filepath)
-        if new_filepath != "":
-            self.entry.delete(0, tk.END)
-            self.entry.insert(0, new_filepath)
-
-
-# toplevelwindow for the junction properties and the video file path
-def video_count_window(app, folder_path_bool):
-    # row index for a simple change of the gui components
-    row_index = 0
-
-    # second window for the settings
-    toplevelwindow = tk.Toplevel(app.window)
-    toplevelwindow.title("Manuel traffic counting settings")
-    toplevelwindow.geometry("550x" + str(9 * 30))
+import tkinter
+import cv2
+import PIL.Image
+import PIL.ImageTk
 
 
 class App:
-    def __init__(self, window, window_title):
+    def __init__(self, window, window_title, video_source=0):
         self.window = window
         self.window.title(window_title)
-        self.window.geometry("1000x950")
+        self.video_source = video_source
 
-        # creates nonexisting canvas to be filled later
-        self.canvas = None
-        self.label_hotkeys = None
+        # open video source
+        self.vid = MyVideoCapture(video_source)
 
-        self.controllbar = Controllbar(self.window, 5, "left")
-        self.sidepanel = Sidepanel(self.window, 5, "right", "ne")
+        # Create a canvas that can fit the above video source size
+        self.canvas = tkinter.Canvas(
+            window, width=self.vid.width, height=self.vid.height)
+        self.canvas.pack()
+        # Button that lets the user take a snapshot
+        self.btn_snapshot = tkinter.Button(
+            window, text="Snapshot", width=50, command=self.snapshot)
+        self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=True)
+
+        self.delay = 15
+        self.update()
 
         self.window.mainloop()
 
+    # If you call the update method once, it will be registered as a callback function, by the window widget after method,
+    # and will be called every delay milliseconds. We can call the update method in the init method before starting the mainloop
+    def update(self):
 
-def mainfunction():
-    App(tk.Tk(), "Traffic Count Tool")
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
+
+        self.window.after(self.delay, self.update)
+
+        self.window.after(self.delay, self.update)
 
 
-if __name__ == "__main__":
-    mainfunction()
+class MyVideoCapture:
+    def __init__(self, video_source=0):
+
+       # Open the video source
+        self.vid = cv2.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
+
+        # Get video source width and height
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    # Release the video source when the object is destroyed
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
+        self.window.mainloop()
+
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                # Return a boolean success flag and the current frame converted to BGR
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return (ret, None)
+
+
+# Create a window and pass it to the Application object
+App(tkinter.Tk(), "Tkinter and OpenCV")
