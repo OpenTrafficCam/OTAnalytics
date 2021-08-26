fb89a30964b44a3ccdad791c35f6e6d38ad28909
hendrik <hendrikgoerner@gmx.de>
2021-08-26 11:52:23 +0200

    rm venv

--- venv/Lib/site-packages/rtree/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-# rtree
-
-Rtree provides Python bindings to libspatialindex for quick
-hyperrectangular intersection queries.
-"""
-__version__ = '0.9.7'
-
-from .index import Rtree, Index  # noqa
