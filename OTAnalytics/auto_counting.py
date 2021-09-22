162ccae2b82269434e5b63b0ddeb585d3b36f63a
hendrik <hendrikgoerner@gmx.de>
2021-09-22 16:38:24 +0200

    output data is shortened

--- OTAnalytics/auto_counting.py
+++ OTAnalytics/auto_counting.py
@@ -13,40 +13,44 @@ import heapq
 
 # %%
 
-# detectors = {}
-# movement_dict = {}
+detectors_dic = {}
+movement_dic = {}
 
 
-# d = {}
+d = {}
 
 
-# detector_dic = open(
-#     "C:/Users/Goerner/Desktop/code/OpenTrafficCam/OTAnalytics/tests/data//line_poly_move_test.OTflow",
-#     "r",
-# )
-# object_dic = open(
-#     "C:/Users/Goerner/Desktop/code/OpenTrafficCam/OTAnalytics/tests/data//object_dic.json",
-#     "r",
-# )
+detector_dic = open(
+    "C:/Users/Goerner/Desktop/code/OpenTrafficCam/OTAnalytics/tests/data//multiple_line_detectors.OTflow",
+    "r",
+)
+object_dic = open(
+    "C:/Users/Goerner/Desktop/code/OpenTrafficCam/OTAnalytics/tests/data//object_dic.json",
+    "r",
+)
 
-# files = open(detector_dic.name, "r")
-# files = files.read()
+files = open(detector_dic.name, "r")
+files = files.read()
 
-# flow_dic = json.loads(files)
-# # use this
-# detectors.update(flow_dic["Detectors"])
-# movement_dict.update(flow_dic["Movements"])
+flow_dic = json.loads(files)
+# use this
+detectors_dic.update(flow_dic["Detectors"])
+movement_dic.update(flow_dic["Movements"])
 
-# files = open(object_dic.name, "r")
-# files = files.read()
 
-# object_dic = json.loads(files)
+files = open(object_dic.name, "r")
+files = files.read()
+
+object_dic = json.loads(files)
+
+# print(object_dic)
+# print(detectors)
 
 
 # %%
 
 
-def dic_to_detector_dataframe(detectors):
+def dic_to_detector_dataframe(detectors_dict):
     """creates a dataframe from detector/flow dictionary, # TODO change to use
     linedetector dic that gets updated when importing flow data
     creates column with LineString-objects for the calculation of
@@ -60,7 +64,7 @@ def dic_to_detector_dataframe(detectors):
     """
     # change dic to dataframe
     detector_df = pd.DataFrame.from_dict(
-        {("Detectors", j): detectors[j] for j in detectors.keys()},
+        {("Detectors", j): detectors_dict[j] for j in detectors_dict.keys()},
         orient="index",
     )
 
@@ -85,7 +89,7 @@ def dic_to_detector_dataframe(detectors):
 
 # %%
 
-# detector_df = dic_to_detector_dataframe(detectors)
+detector_df = dic_to_detector_dataframe(detectors_dic)
 # print(detector_df)
 
 # %%
@@ -125,7 +129,7 @@ def dic_to_object_dataframe(object_dic):
 
 # %%
 
-# object_df_validated_copy = dic_to_object_dataframe(object_dic)
+object_df_validated_copy = dic_to_object_dataframe(object_dic)
 
 
 # %%
@@ -161,8 +165,6 @@ def calculate_intersections(detector_df, object_df_validated_copy):
         # returns coordinates from intersections as point object
         point_geometry = track_geometry.intersection(detector_shapely_geometry)
 
-        print(point_geometry)
-
         object_df_validated_copy[index + "intersectcoordinates"] = point_geometry
         object_df_validated_copy[index] = bool_intersect
 
@@ -178,15 +180,15 @@ def calculate_intersections(detector_df, object_df_validated_copy):
 
 
 # %%
-# object_df_validated_copy = calculate_intersections(
-#     detector_df, object_df_validated_copy
-# )
+object_df_validated_copy = calculate_intersections(
+    detector_df, object_df_validated_copy
+)
 
 
 # %%
 
 
-def find_intersection_order(fps, object_df_validated_copy, detectors):
+def find_intersection_order(fps, object_df_validated_copy, detector_dict):
     """First create necessary columns (Crossing_Gate/Frame; Movement; Movement_name)
 
     Second find nearest point (second nearest point) on Linestring compared
@@ -196,9 +198,8 @@ def find_intersection_order(fps, object_df_validated_copy, detectors):
 
 
     Args:
-        object_df_validated_copy ([type]): [description]
-        linedetectors ([type]): [description]
-
+        object_df_validated_copy ([type]): dataframe
+        linedetectors ([type]): dictionary
     Returns:
         [type]: [description]
     """
@@ -211,9 +212,10 @@ def find_intersection_order(fps, object_df_validated_copy, detectors):
     object_df_validated_copy["Time_crossing_exit"] = ""
 
     for object_id, row in object_df_validated_copy.iterrows():
-        for detector in detectors:
-            # Condition if detector was crossed by objecttrack
-            # Dont change to "is True"!!
+        # use dict
+        for detector in detector_dict:
+            #         # Condition if detector was crossed by objecttrack
+            #         # Dont change to "is True"!!
             if object_df_validated_copy.loc[object_id][detector] == True:
 
                 # shapely Linestring
@@ -228,6 +230,7 @@ def find_intersection_order(fps, object_df_validated_copy, detectors):
 
                 # get second nearest point(
                 # second nearest point = coordinate on linestring)
+                # nearest point is crossing on detector/ second nearest is track coord
                 nearest, second_nearest = heapq.nsmallest(
                     2, line_points, key=intersection_point.distance
                 )
@@ -240,7 +243,7 @@ def find_intersection_order(fps, object_df_validated_copy, detectors):
                 # index at which the second closest points are
                 index_number = raw_coords.index(point_raw_coords)
 
-                # with the index number you can also get the second from gatecrossing
+                # with the index number you can also get the frame from gatecrossing
                 crossing_frame = object_df_validated_copy.loc[object_id]["Frame"][
                     index_number
                 ]
@@ -269,6 +272,7 @@ def find_intersection_order(fps, object_df_validated_copy, detectors):
                 concatted_sorted_detector_list = [
                     item for sublist in t for item in sublist
                 ]
+                # deletes extra brackets (list)
                 del concatted_sorted_detector_list[1::2]
 
                 object_df_validated_copy.at[object_id, "Time_crossing_entrace"] = (
@@ -276,10 +280,17 @@ def find_intersection_order(fps, object_df_validated_copy, detectors):
                     / fps
                 )
 
-                object_df_validated_copy.at[object_id, "Time_crossing_exit"] = (
+                if object_df_validated_copy.at[object_id, "Time_crossing_entrace"] != (
                     object_df_validated_copy.at[object_id, "Crossing_Gate/Frame"][-1][1]
                     / fps
-                )
+                ):
+
+                    object_df_validated_copy.at[object_id, "Time_crossing_exit"] = (
+                        object_df_validated_copy.at[object_id, "Crossing_Gate/Frame"][
+                            -1
+                        ][1]
+                        / fps
+                    )
 
                 # list = Movement (only detectors not seconds)
                 object_df_validated_copy.at[
@@ -288,45 +299,26 @@ def find_intersection_order(fps, object_df_validated_copy, detectors):
 
     return object_df_validated_copy
 
-    # print(object_df_validated_copy)
-
-    # if object_df_validated_copy.at[object_id, "Movement"]:
-
-    #     object_df_validated_copy.at[object_id, "Movement"].append(detector)
-
-    # else:
-    #     object_df_validated_copy.at[object_id, "Movement"] = [detector]
-
 
 # %%
-# oject_df_validated_copy = find_intersection_order(
-#     20, object_df_validated_copy, detectors
-# )
+object_df_validated_copy = find_intersection_order(
+    20, object_df_validated_copy, detectors_dic
+)
 
 
 # %%
 
 
 def assign_movement(movement_dict, object_df_validated_copy):
+    """Compares movements and associated detectors with sorted crossing list
 
-    # object_df_intersections = object_df_validated_copy.iloc[:, 14:] # 13, 14 or 16
-    # very buggy and i dont know why
+    Args:
+        movement_dict ([dictionary]): dictionary with movements
+        object_df_validated_copy ([dataframe]): validated object dataframe
 
-    # object_dic = object_df_intersections.to_dict('index')
-
-    # for nested_dic in
-    #  object_dic:
-
-    #     calculated_movement = {k: v for k, v in object_dic[
-    #     nested_dic].items() if v == v}
-
-    #     sorted_calculated_movement = sorted(calculated_movement,
-    #     key=calculated_movement.get)
-
-    #     sorted_calculated_movement= [w.replace(
-    #     '_distance', '') for w in sorted_calculated_movement]
-
-    #     # compare with movement dictionary
+    Returns:
+        object_df_validated_copy ([dataframe]): validated object dataframe with movements
+    """
 
     for object_id, j in object_df_validated_copy.iterrows():
 
@@ -347,27 +339,7 @@ def assign_movement(movement_dict, object_df_validated_copy):
 
 
 # %%
-# object_df_validated_copy = assign_movement(movement_dict, object_df_validated_copy)
-
-
-# %%
-# def hash_crossing_second(
-# object_df_validated_copy):
-#     object_df_validated_copy[
-#     "Second_crossing_entrace"] = object_df_validated_copy.apply(
-#         lambda entrance_second:entrance_second['Crossing_Gate/Frame'])
-
-#     #object_df_validated_copy["Second_crossing_exit"]
-
-#     return object_df_validated_copy
-
-
-# %%
-
-# object_df_validated_copy = hash_crossing_second(object_df_validated_copy)
-
-# print(object_df_validated_copy)
-
+object_df_validated_copy = assign_movement(movement_dic, object_df_validated_copy)
 
 # %%
 def safe_to_csv(process_object):
@@ -385,32 +357,63 @@ def safe_to_csv(process_object):
 
 
 # %%
+
+
+def clean_dataframe(object_df_validated_copy):
+
+    cleaned_automated_counting = object_df_validated_copy.loc[
+        :,
+        [
+            "Class",
+            "Movement",
+            "Movement_name",
+            "Time_crossing_entrace",
+            "Time_crossing_exit",
+        ],
+    ]
+
+    return cleaned_automated_counting
+
+
 # %%
 
 
-def automated_counting(fps, movement_dict, linedetectors_dict, object_dict):
+def automated_counting(fps, movement_dic, detector_dic, object_dic):
     """calls previous funtions
 
     Args:
         detector_dict (dictionary): dictionary with detectors
         object_dict (dictionary): dictionary with obejcts (at least 3 detections)
+
     """
 
-    if gui_dict["tracks_imported"] and linedetectors_dict and movement_dict:
-        detector_dataframe = dic_to_detector_dataframe(linedetectors_dict)
-        object_df_validated_copy = dic_to_object_dataframe(object_dict)
-        processed_object = calculate_intersections(
-            detector_dataframe, object_df_validated_copy
-        )
+    # if gui_dict["tracks_imported"] and detector_dic and movement_dic:
+    detector_dataframe = dic_to_detector_dataframe(detector_dic)
+    object_df_validated_copy = dic_to_object_dataframe(object_dic)
+    processed_object = calculate_intersections(
+        detector_dataframe, object_df_validated_copy
+    )
 
-        # TODO doesnt return right dataframe
+    print("succesfull")
 
-        processed_object = find_intersection_order(
-            fps, object_df_validated_copy, linedetectors_dict
-        )
-        processed_object = assign_movement(movement_dict, processed_object)
+    # TODO doesnt return right dataframe
 
-        safe_to_csv(processed_object)
+    processed_object = find_intersection_order(fps, processed_object, detector_dic)
+    processed_object = assign_movement(movement_dic, processed_object)
+    cleaned_object_dataframe = clean_dataframe(processed_object)
+
+    safe_to_csv(cleaned_object_dataframe)
+
+    return cleaned_object_dataframe
+
+
+# %%
+
+# cleaned_object_dataframe = automated_counting(
+#     25, movement_dic, detectors_dic, object_dic
+# )
+
+# print(cleaned_object_dataframe)
 
 
 # %%
