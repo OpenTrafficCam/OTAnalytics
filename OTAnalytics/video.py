349c790f8d8b2970e4674a2e247af7445b7133b6
hgoerner <hendrik.goerner@tu-dresden.de>
2021-12-22 16:17:33 +0100

    filestream class

--- OTAnalytics/video.py
+++ OTAnalytics/video.py
@@ -3,6 +3,17 @@ from tkinter import filedialog
 import cv2
 from PIL import Image, ImageTk
 
+# import the necessary packages
+from threading import Thread
+import sys
+import time
+
+
+import numpy as np
+
+if sys.version_info >= (3, 0):
+    from queue import Queue
+
 
 def load_video_and_frame():
     """ask for videofile via dialogue
@@ -14,55 +25,143 @@ def load_video_and_frame():
     video_source = filedialog.askopenfile(
         filetypes=[("Videofiles", "*.mkv"), ("Videofiles", "*.mp4")]
     )
-    video_source = video_source.name
+    filepath = video_source.name
+    print("filepath: " + filepath)
 
-    return Video(video_source)
+    return Video(filepath, queue_size=128)
 
 
-class Video:
+class FileVideoStream:
+    def __init__(self, path, transform=None, queue_size=128):
+        # initialize the file video stream along with the boolean
+        # used to indicate if the thread should be stopped or not
+        self.stream = cv2.VideoCapture(path)
+        self.stopped = False
+        self.transform = transform
+
+        # initialize the queue used to store frames read from
+        # the video file
+        self.Q = Queue(maxsize=queue_size)
+        # intialize thread
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+
+    def start(self):
+        # start a thread to read frames from the file video stream
+        self.thread.start()
+        return self
+
+    def update(self):
+        # keep looping infinitely
+        while True:
+            # if the thread indicator variable is set, stop the
+            # thread
+            if self.stopped:
+                break
+
+            # otherwise, ensure the queue has room in it
+            if not self.Q.full():
+                # read the next frame from the file
+                (grabbed, frame) = self.stream.read()
+
+                # if the `grabbed` boolean is `False`, then we have
+                # reached the end of the video file
+                if not grabbed:
+                    self.stopped = True
+
+                # if there are transforms to be done, might as well
+                # do them on producer thread before handing back to
+                # consumer thread. ie. Usually the producer is so far
+                # ahead of consumer that we have time to spare.
+                #
+                # Python is not parallel but the transform operations
+                # are usually OpenCV native so release the GIL.
+                #
+                # Really just trying to avoid spinning up additional
+                # native threads and overheads of additional
+                # producer/consumer queues since this one was generally
+                # idle grabbing frames.
+                if self.transform:
+                    frame = self.transform(frame)
+
+                # add the frame to the queue
+                self.Q.put(frame)
+            else:
+                time.sleep(0.1)  # Rest for 10ms, we have a full queue
+
+        self.stream.release()
+
+    def read(self):
+        # return next frame in the queue
+        return self.Q.get()
+
+    # Insufficient to have consumer use while(more()) which does
+    # not take into account if the producer has reached end of
+    # file stream.
+    def running(self):
+        return self.more() or not self.stopped
+
+    def more(self):
+        # return True if there are still frames in the queue. If stream is not stopped, try to wait a moment
+        tries = 0
+        while self.Q.qsize() == 0 and not self.stopped and tries < 5:
+            time.sleep(0.1)
+            tries += 1
+
+        return self.Q.qsize() > 0
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+        # wait until stream resources are released (producer thread might be still grabbing frame)
+        self.thread.join()
+
+
+class Video(FileVideoStream):
     """Videoclass that gets created on importing video."""
 
     # object which contains relevant information of the video
-    def __init__(self, filepath):
+    def __init__(self, filepath, **kwargs):
+        super().__init__(filepath, **kwargs)
 
-        # self.id = id
-        self.filepath = filepath
         self.filename = filepath.split("/")[-1]
 
+        # start a thread to read frames from the file video stream
+        self.start()
+        time.sleep(1)
+
         # opens video source
-        self.cap = cv2.VideoCapture(self.filepath)
-        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        # self.cap = cv2.VideoCapture(self.filepath)
+        self.fps = self.stream.get(cv2.CAP_PROP_FPS)
 
         # retrieve dimensions of video
-        self.width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.width = self.stream.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-        self.totalframecount = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.totalframecount = int(self.stream.get(cv2.CAP_PROP_FRAME_COUNT))
 
         # time between two frames
         self.frame_delay = 1 / self.fps
 
         self.current_frame = 0
 
-        # self.buffercount = 0
-
-        self.frame_list = {}
-
-        self.buffer_video()
-
     def get_frame(self, np_image):
 
         # when imported set current frame to 0
         # self.cap.set(1, self.current_frame)
 
-        if self.current_frame not in self.frame_list:
-            self.buffer_video()
+        # if self.current_frame not in self.frame_list:
+        #     self.buffer_video()
 
         print("Frame: " + str(self.current_frame))
 
-        self.np_image = cv2.cvtColor(
-            self.frame_list[self.current_frame], cv2.COLOR_BGR2RGB
-        )  # to RGB
+        self.stream.set(1, self.current_frame)
+
+        # self.set(1, self.current_frame)
+
+        frame = self.read()
+
+        self.np_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # to RGB
 
         if np_image:
 
@@ -79,24 +178,20 @@ class Video:
 
         return self.ph_image
 
-    def buffer_video(self):
 
-        self.frame_list = {}
+# path = "C:/Users/Goerner/Desktop/code/OpenTrafficCam/OTAnalytics/tests/test-data/input/radeberg.mp4"
 
-        print("buffering video")
+# video = Video(path)
 
-        self.buffercount = int(self.current_frame / 500)
+# print(video.fps)
 
-        self.cap.set(1, self.buffercount * 500)
+# while video.more():
 
-        # buffer next 500 frames to play video
-        for i in range(self.buffercount * 500, (self.buffercount + 1) * 500):
-            ret, frame = self.cap.read()
-            self.frame_list[i] = frame
+#     frame = video.read()
 
-        self.cap.set(1, self.buffercount * 500)
+#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # buffer previous 500 frames to rewind video
-        for i in range(self.buffercount * 500, (self.buffercount - 1) * 500):
-            ret, frame = self.cap.read()
-            self.frame_list[i] = frame
+#     cv2.imshow("Frame", frame)
+#     time.sleep(video.frame_delay)
+
+#     cv2.waitKey(1)
