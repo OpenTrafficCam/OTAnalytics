46cfbb03f750600e9371a9fb07d9c734e3f0b37c
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2023-03-06 18:47:50 +0100

    Read ottrk file

--- OTAnalytics/domain/track.py
+++ OTAnalytics/domain/track.py
@@ -1,33 +1,100 @@
-from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
 from typing import Iterable, Optional
 
+from pydantic import BaseModel, Field, validator
 
-@dataclass(frozen=True)
-class Detection:
-    classification: str
-    confidence: float
-    x: float
-    y: float
-    w: float
-    h: float
-    frame: int
+import OTAnalytics.plugin_parser.ottrk_dataformat as ottrk_format
+
+
+class Detection(BaseModel, frozen=True, allow_population_by_field_name=True):
+    """Represents a detection belonging to a `Track`.
+
+    The detection uses the xywh bounding box format.
+
+    Extends from pydantic.BaseModel.
+    IMPORTANT: Instantiation of this class is only possible by using named parameters.
+
+    Raises:
+        ValueError: confidence not in [0,1]
+        ValueError: x < 0
+        ValueError: y < 0
+        ValueError: w < 0
+        ValueError: h < 0
+        ValueError: frame < 0
+        ValueError: track_id < 0
+
+
+    Args:
+        classification (str): class of detection.
+        confidence (float): the confidence.
+        x (float): the x coordinate component of the bounding box.
+        y (float): the y coordinate component of the bounding box.
+        w (float): the width component of the bounding box.
+        h (float): the height component of the bounding box.
+        frame (int): the frame that the detection belongs to.
+        occurrence (datetime): the time of the detection's occurence.
+        input_file_path (Path): absolute path to otdet that the detection belongs to
+        at the time of its creation.
+        interpolated_detection (bool): whether this detection is interpolated.
+        track_id (int): the track id this detection belongs to.
+    """
+
+    classification: str = Field(alias=ottrk_format.CLASS)
+    confidence: float = Field(gt=0, le=1, description="must be between range [0,1]")
+    x: float = Field(ge=0, description="must be greater equal than 0")
+    y: float = Field(ge=0, description="must be greater equal than 0")
+    w: float = Field(ge=0, description="must be greater equal than 0")
+    h: float = Field(ge=0, description="must be greater equal than 0")
+    frame: int = Field(gt=0, description="must be greater than 0")
     occurrence: datetime
     input_file_path: Path
-    interpolated_detection: bool
-    track_id: int
+    interpolated_detection: bool = Field(alias=ottrk_format.INTERPOLATED_DETECTION)
+    track_id: int = Field(
+        gt=0,
+        description="Track ID must be greater equal than 0",
+        alias=ottrk_format.TRACK_ID,
+    )
 
 
-@dataclass(frozen=True)
-class Track:
-    id: int
+class Track(BaseModel, frozen=True, allow_population_by_field_name=True):
+    """Represents the the track of an object as seen in the task of object tracking
+    (computer vision).
+
+    Extends from pydantic.BaseModel.
+    IMPORTANT: Instantiation of this class is only possible by using named parameters.
+
+    Args:
+        id (int): the track id.
+        detections (list[Detection]): the detections belonging to this track.
+
+    Raises:
+        ValueError: if detections are not sorted by `frame`.
+        ValueError: if an empty detctions list has been passed.
+    """
+
+    id: int = Field(gt=0, description="id must be a number greater than 0")
     detections: list[Detection]
 
+    @validator("detections")
+    def detections_must_be_in_right_order(
+        cls, detections: list[Detection]
+    ) -> list[Detection]:
+        if detections != sorted(detections, key=lambda det: det.frame):
+            raise ValueError("detections must be sorted by frame number")
+        return detections
 
-class TrackRepository:
-    def __init__(self) -> None:
-        self.tracks: dict[int, Track] = {}
+    @validator("detections")
+    def detections_must_not_be_empty(
+        cls, detections: list[Detection]
+    ) -> list[Detection]:
+        if not detections:
+            raise ValueError("must not be empty")
+        return detections
+
+
+class TrackRepository(BaseModel):
+    tracks: dict[int, Track] = {}
 
     def add(self, track: Track) -> None:
         self.tracks[track.id] = track
--- OTAnalytics/plugin_parser/otvision_parser.py
+++ OTAnalytics/plugin_parser/otvision_parser.py
@@ -1,9 +1,61 @@
+import bz2
+import json
+from datetime import datetime
 from pathlib import Path
 
-from application.datastore import TrackParser
-from domain.track import Track
+import OTAnalytics.plugin_parser.ottrk_dataformat as ottrk_format
+from OTAnalytics.application.datastore import TrackParser
+from OTAnalytics.domain.track import Detection, Track
 
 
 class OttrkParser(TrackParser):
-    def parse(self, file: Path) -> list[Track]:
-        return []
+    def parse(self, ottrk_file: Path) -> list[Track]:
+        ottrk_dict = self._parse_bz2(ottrk_file)
+        dets_list: list[dict] = ottrk_dict[ottrk_format.DATA][ottrk_format.DETECTIONS]
+        tracks = self._parse_tracks(dets_list)
+        return tracks
+
+    def _parse_bz2(self, p: Path) -> dict:
+        """Parse JSON bz2.
+
+        Args:
+            p (Path): Path to bz2 JSON.
+
+        Returns:
+            dict: The content of the JSON file.
+        """
+        with bz2.open(p, "r") as f:
+            _dict = json.load(f)
+            return _dict
+
+    def _parse_tracks(self, d: list[dict]) -> list[Track]:
+        tracks_dict = self._parse_detections(d)
+        tracks: list[Track] = []
+        for track_id, detections in tracks_dict.items():
+            sort_dets_by_frame = sorted(detections, key=lambda det: det.frame)
+            tracks.append(Track(id=track_id, detections=sort_dets_by_frame))
+        return tracks
+
+    def _parse_detections(self, det_list: list[dict]) -> dict[int, list[Detection]]:
+        tracks_dict: dict[int, list[Detection]] = {}
+        # Group detections by track id
+        for det_dict in det_list:
+            det = Detection(
+                classification=det_dict[ottrk_format.CLASS],
+                confidence=det_dict[ottrk_format.CONFIDENCE],
+                x=det_dict[ottrk_format.X],
+                y=det_dict[ottrk_format.Y],
+                w=det_dict[ottrk_format.W],
+                h=det_dict[ottrk_format.H],
+                frame=det_dict[ottrk_format.FRAME],
+                occurrence=datetime.strptime(
+                    det_dict[ottrk_format.OCCURENCE], ottrk_format.DATE_FORMAT
+                ),
+                input_file_path=det_dict[ottrk_format.INPUT_FILE_PATH],
+                interpolated_detection=det_dict[ottrk_format.INTERPOLATED_DETECTION],
+                track_id=det_dict[ottrk_format.TRACK_ID],
+            )
+            if not tracks_dict.get(det.track_id):
+                tracks_dict[det.track_id] = []
+            tracks_dict[det.track_id].append(det)
+        return tracks_dict
