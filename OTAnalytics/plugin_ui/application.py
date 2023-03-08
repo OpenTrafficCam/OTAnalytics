94d2cb61f84aea2f769740b481e2b7c627c6793b
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2023-03-08 18:55:58 +0100

    Add ButtonLoadTracks

--- OTAnalytics/plugin_ui/application.py
+++ OTAnalytics/plugin_ui/application.py
@@ -1,17 +1,49 @@
 import tkinter
 from pathlib import Path
+from tkinter.filedialog import askopenfilename
+from typing import Any
 
 import customtkinter
-from customtkinter import CTk, CTkButton
+from customtkinter import CTk, CTkButton, CTkFrame
 
 from OTAnalytics.application.datastore import Datastore
 from OTAnalytics.plugin_parser.otvision_parser import OttrkParser
+
+
+class ButtonLoadTracks(CTkButton):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+
+        self.bind("<ButtonRelease-1>", self.on_click)
+
+    def on_click(self, events: Any) -> None:
+        self.tracks_file = askopenfilename(
+            title="Load tracks file", filetypes=[("tracks file", "*.ottrk")]
+        )
+        print(self.tracks_file)
+
+
+class FrameFiles(CTkFrame):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.layout()
+
+    def layout(self) -> None:
+        self.button_load_tracks = ButtonLoadTracks(
+            master=self,
+            text="Load tracks",
+        )
+        self.button_load_tracks.pack()
 
 
 class OTAnalyticsApplication:
     def __init__(self) -> None:
         self.datastore: Datastore
         self.app: CTk
+
+    def layout(self) -> None:
+        self.frame_files = FrameFiles(master=self.app)
+        self.frame_files.pack()
 
     def add_track_of_file(self) -> None:
         track_file = Path("")  # TODO read from file chooser
@@ -35,7 +67,9 @@ class OTAnalyticsApplication:
         self.app = CTk()
         self.app.geometry("800x600")
 
-        self.add_track_loader()
+        # self.add_track_loader()
+
+        self.layout()
 
         self.app.mainloop()
 
