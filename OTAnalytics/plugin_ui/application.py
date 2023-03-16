d7d8059519241b5057435f91dacad0bb9afedd85
Lars Briem <lars.briem@platomo.de>
2023-03-16 21:02:22 +0100

    Fix review comment by separating responsibilities

--- OTAnalytics/plugin_ui/application.py
+++ OTAnalytics/plugin_ui/application.py
@@ -15,8 +15,8 @@ from OTAnalytics.plugin_parser.otvision_parser import (
 
 
 class OTAnalyticsApplication:
-    def __init__(self) -> None:
-        self._datastore: Datastore
+    def __init__(self, datastore: Datastore) -> None:
+        self._datastore: Datastore = datastore
 
     def add_tracks_of_file(self, track_file: Path) -> None:
         self._datastore.load_track_file(file=track_file)
@@ -25,26 +25,16 @@ class OTAnalyticsApplication:
         self._datastore.load_section_file(file=sections_file)
 
     def start(self) -> None:
-        self.setup_application()
         self.start_internal()
 
     @abstractmethod
     def start_internal(self) -> None:
         pass
 
-    def setup_application(self) -> None:
-        """
-        Build all required objects and inject them where necessary
-        """
-        track_parser = OttrkParser()
-        section_parser = OtsectionParser()
-        video_parser = OttrkVideoParser()
-        self._datastore = Datastore(track_parser, section_parser, video_parser)
-
 
 class OTAnalyticsCli(OTAnalyticsApplication):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, datastore: Datastore) -> None:
+        super().__init__(datastore)
 
     def start_internal(self) -> None:
         # TODO parse config and add track and section files
@@ -52,61 +42,80 @@ class OTAnalyticsCli(OTAnalyticsApplication):
 
 
 class OTAnalyticsGui(OTAnalyticsApplication):
-    def __init__(self) -> None:
+    def __init__(self, datastore: Datastore) -> None:
+        super().__init__(datastore)
         self._app: CTk
 
-    def load_tracks_in_file(self) -> None:
+    def _load_tracks_in_file(self) -> None:
         track_file = Path("")  # TODO read from file chooser
         self._datastore.load_track_file(file=track_file)
 
-    def load_sections_in_file(self) -> None:
+    def _load_sections_in_file(self) -> None:
         section_file = Path("")  # TODO read from file chooser
         self._datastore.load_section_file(file=section_file)
 
-    def start(self) -> None:
-        self.setup_application()
-        self.show_gui()
+    def start_internal(self) -> None:
+        self._show_gui()
 
-    def show_gui(self) -> None:
+    def _show_gui(self) -> None:
         customtkinter.set_appearance_mode("System")
         customtkinter.set_default_color_theme("blue")
 
         self._app = CTk()
         self._app.geometry("800x600")
 
-        self.add_track_loader()
-        self.add_section_loader()
+        self._add_track_loader()
+        self._add_section_loader()
 
         self._app.mainloop()
 
-    def add_track_loader(self) -> None:
+    def _add_track_loader(self) -> None:
         button = CTkButton(
             master=self._app,
             text="Read tracks",
-            command=self.load_tracks_in_file,
+            command=self._load_tracks_in_file,
         )
         button.place(relx=0.25, rely=0.5, anchor=tkinter.CENTER)
 
-    def add_section_loader(self) -> None:
+    def _add_section_loader(self) -> None:
         button = CTkButton(
             master=self._app,
             text="Read sections",
-            command=self.load_sections_in_file,
+            command=self._load_sections_in_file,
         )
         button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
-    def add_section_button(self) -> None:
+    def _add_section_button(self) -> None:
         button = CTkButton(
             master=self._app,
             text="Add sections",
-            command=self.add_section,
+            command=self._add_section,
         )
         button.place(relx=0.75, rely=0.5, anchor=tkinter.CENTER)
 
-    def add_section(self) -> None:
+    def _add_section(self) -> None:
         section: Section = LineSection(
             id="north",
             start=Coordinate(0, 1),
             end=Coordinate(2, 3),
         )
         self._datastore.add_section(section)
+
+
+class ApplicationStarter:
+    def start_gui(self) -> None:
+        datastore = self.create_datastore()
+        OTAnalyticsGui(datastore).start()
+
+    def start_cli(self) -> None:
+        datastore = self.create_datastore()
+        OTAnalyticsCli(datastore).start()
+
+    def create_datastore(self) -> Datastore:
+        """
+        Build all required objects and inject them where necessary
+        """
+        track_parser = OttrkParser()
+        section_parser = OtsectionParser()
+        video_parser = OttrkVideoParser()
+        return Datastore(track_parser, section_parser, video_parser)
