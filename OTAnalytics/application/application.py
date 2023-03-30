bfe495dcdd5d19605a07274d8663497fcc410a3b
Lars Briem <lars.briem@platomo.de>
2023-03-30 13:08:36 +0200

    Document code

--- OTAnalytics/application/application.py
+++ OTAnalytics/application/application.py
@@ -7,6 +7,10 @@ from OTAnalytics.domain.track import TrackId, TrackImage
 
 
 class OTAnalyticsApplication:
+    """
+    Entrypoint for calls from the UI.
+    """
+
     def __init__(
         self, datastore: Datastore, track_state: TrackState, section_state: SectionState
     ) -> None:
@@ -16,14 +20,39 @@ class OTAnalyticsApplication:
         self._connect_observers()
 
     def _connect_observers(self) -> None:
+        """
+        Connect the observers with the repositories to listen to domain object changes.
+        """
         self._datastore.register_tracks_observer(self.track_state)
         self._datastore.register_sections_observer(self.section_state)
 
     def add_tracks_of_file(self, track_file: Path) -> None:
+        """
+        Load a single track file.
+
+        Args:
+            track_file (Path): file in ottrk format
+        """
         self._datastore.load_track_file(file=track_file)
 
     def add_sections_of_file(self, sections_file: Path) -> None:
+        """
+        Load sections from a sections file.
+
+        Args:
+            sections_file (Path): file in sections format
+        """
         self._datastore.load_section_file(file=sections_file)
 
     def get_image_of_track(self, track_id: TrackId) -> Optional[TrackImage]:
+        """
+        Retrieve an image for the given track.
+
+        Args:
+            track_id (TrackId): identifier for the track
+
+        Returns:
+            Optional[TrackImage]: an image of the track if the track is available and
+            the image can be loaded
+        """
         return self._datastore.get_image_of_track(track_id)
--- OTAnalytics/application/datastore.py
+++ OTAnalytics/application/datastore.py
@@ -92,29 +92,74 @@ class Video:
 
 
 class VideoRepository:
+    """
+    Repository containing the videos per track.
+    """
+
     def __init__(self) -> None:
         self._videos: dict[TrackId, Video] = {}
 
     def add(self, track_id: TrackId, video: Video) -> None:
+        """
+        Add a video for a track id.
+
+        Args:
+            track_id (TrackId): id of the track
+            video (Video): video containing the track
+        """
         self._videos[track_id] = video
 
     def add_all(self, track_ids: Iterable[TrackId], videos: Iterable[Video]) -> None:
+        """
+        Add all videos for all tracks.
+
+        Args:
+            track_ids (Iterable[TrackId]): track ids to be added
+            videos (Iterable[Video]): videos per track id to be added
+        """
         for track_id, video in zip(track_ids, videos):
             self.add(track_id, video)
 
     def get_video_for(self, track_id: TrackId) -> Optional[Video]:
+        """
+        Retrieve a video for the given track id.
+
+        Args:
+            track_id (TrackId): id of the track to get a video for
+
+        Returns:
+            Optional[Video]: video of the track if a video exists for the track
+        """
         return self._videos.get(track_id)
 
 
 class VideoParser(ABC):
+    """
+    Parse the information about videos from a track file
+    """
+
     @abstractmethod
     def parse(
         self, file: Path, track_ids: list[TrackId]
     ) -> Tuple[list[TrackId], list[Video]]:
+        """
+        Parse the given file in ottrk format and retrieve video information from it
+
+        Args:
+            file (Path): file in ottrk format
+            track_ids (list[TrackId]): track ids to get videos for
+
+        Returns:
+            Tuple[list[TrackId], list[Video]]: track ids and the corresponding videos
+        """
         pass
 
 
 class Datastore:
+    """
+    Central element to hold data in the application.
+    """
+
     def __init__(
         self,
         track_parser: TrackParser,
@@ -132,12 +177,30 @@ class Datastore:
         self._video_repository = VideoRepository()
 
     def register_tracks_observer(self, observer: TrackListObserver) -> None:
+        """
+        Listen to changes in the track repository.
+
+        Args:
+            observer (TrackListObserver): listener to be notified about changes
+        """
         self._track_repository.register_tracks_observer(observer)
 
     def register_sections_observer(self, observer: SectionListObserver) -> None:
+        """
+        Listen to changes in the section repository.
+
+        Args:
+            observer (SectionListObserver): listener to be notified about changes
+        """
         self._section_repository.register_sections_observer(observer)
 
     def load_track_file(self, file: Path) -> None:
+        """
+        Load and parse the given track file together with the corresponding video file.
+
+        Args:
+            file (Path): file in ottrk format
+        """
         tracks = self._track_parser.parse(file)
         track_ids = [track.id for track in tracks]
         track_ids, videos = self._video_parser.parse(file, track_ids)
@@ -145,25 +208,59 @@ class Datastore:
         self._track_repository.add_all(tracks)
 
     def load_section_file(self, file: Path) -> None:
+        """
+        Load sections from the given files and store them in the section repository.
+
+        Args:
+            file (Path): file to load sections from
+        """
         sections = self._section_parser.parse(file)
         self._section_repository.add_all(sections)
 
     def save_section_file(self, file: Path) -> None:
+        """
+        Save sections from the repository in a section file.
+
+        Args:
+            file (Path): file to save sections to
+        """
         self._section_parser.serialize(
             self._section_repository.get_all(),
             file=file,
         )
 
     def save_event_list_file(self, file: Path) -> None:
+        """
+        Save events from the event list in an event file.
+
+        Args:
+            file (Path): file to save events to
+        """
         self._event_list_parser.serialize(
             self._event_repository.get_all(),
             file=file,
         )
 
     def add_section(self, section: Section) -> None:
+        """
+        Add a single section to the repository.
+
+        Args:
+            section (Section): section to add
+        """
         self._section_repository.add(section)
 
     def get_image_of_track(self, track_id: TrackId) -> Optional[TrackImage]:
+        """
+        Retrieve an image for the given track.
+
+        Args:
+            track_id (TrackId): identifier for the track
+
+        Returns:
+            Optional[TrackImage]: an image of the track if the track is available and
+            the image can be loaded
+        """
         if video := self._video_repository.get_video_for(track_id):
             return video.get_frame(0)
         return None
