1d1501f086cff5823c581486974d639b69395687
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2023-03-31 17:12:41 +0200

    Move event and track builder for testing to conftest to make them accessible to all other test modules

--- tests/OTAnalytics/domain/test_track.py
+++ tests/OTAnalytics/domain/test_track.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from pathlib import Path
-from typing import Optional
 from unittest.mock import Mock
 
 import pytest
@@ -14,6 +13,7 @@ from OTAnalytics.domain.track import (
     TrackId,
     TrackRepository,
 )
+from tests.conftest import TrackBuilder
 
 
 @pytest.fixture
@@ -50,81 +50,6 @@ def valid_detection(valid_detection_dict: dict) -> Detection:
         ],
         track_id=valid_detection_dict[ottrk_format.TRACK_ID],
     )
-
-
-class DataBuilder:
-    def __init__(
-        self,
-        confidence: float,
-        x: float,
-        y: float,
-        w: float,
-        h: float,
-        occurrence: datetime,
-        input_file_path: Path,
-        interpolated_detection: bool,
-        track_id: TrackId,
-        frame: Optional[int] = None,
-        track_class: Optional[str] = None,
-        detection_class: Optional[str] = None,
-    ) -> None:
-        self._track_class = track_class
-        self._detections: list[Detection] = []
-        self._detection_class = detection_class
-        self._confidence = confidence
-        self._x = x
-        self._y = y
-        self._w = w
-        self._h = h
-        self._frame = frame
-        self._occurrence = occurrence
-        self._input_file_path = input_file_path
-        self._interpolated_detection = interpolated_detection
-        self._track_id = track_id
-
-    def build_track(self) -> Track:
-        if self._track_class is None:
-            raise ValueError("track ")
-
-        return Track(self._track_id, self._track_class, self._detections)
-
-    def build_detections(self) -> list[Detection]:
-        return self._detections
-
-    def append_detection(self) -> None:
-        if self._detection_class is None:
-            raise ValueError("classification not set")
-
-        if self._frame is None:
-            raise ValueError("frame not set")
-
-        self._detections.append(
-            Detection(
-                classification=self._detection_class,
-                confidence=self._confidence,
-                x=self._x,
-                y=self._y,
-                w=self._w,
-                h=self._h,
-                frame=self._frame,
-                occurrence=self._occurrence,
-                input_file_path=self._input_file_path,
-                interpolated_detection=self._interpolated_detection,
-                track_id=self._track_id,
-            )
-        )
-
-    def add_detection_class(self, classification: str) -> None:
-        self._detection_class = classification
-
-    def add_confidence(self, confidence: float) -> None:
-        self._confidence = confidence
-
-    def add_frame(self, frame: int) -> None:
-        self._frame = frame
-
-    def add_track_class(self, classification: str) -> None:
-        self._track_class = classification
 
 
 class TestDetection:
@@ -213,18 +138,10 @@ class TestTrack:
 
 class TestCalculateTrackClassificationByMaxConfidence:
     def test_calculate(self) -> None:
-        builder = DataBuilder(
+        builder = TrackBuilder(
             confidence=0.8,
-            x=0.0,
-            y=0.0,
             w=10,
             h=10,
-            frame=1,
-            occurrence=datetime(2022, 1, 1, 0, 0, 0, 0),
-            input_file_path=Path("mypath_postfix.otdet"),
-            interpolated_detection=False,
-            track_id=TrackId(1),
-            detection_class="car",
         )
         builder.append_detection()
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,190 @@
 import shutil
+from dataclasses import dataclass
+from datetime import datetime
 from pathlib import Path
 from typing import Generator, TypeVar
 
 import pytest
 
+from OTAnalytics.domain.event import Event, EventType
+from OTAnalytics.domain.geometry import DirectionVector2D, ImageCoordinate
 from OTAnalytics.domain.section import Section
-from OTAnalytics.domain.track import CalculateTrackClassificationByMaxConfidence, Track
+from OTAnalytics.domain.track import (
+    CalculateTrackClassificationByMaxConfidence,
+    Detection,
+    Track,
+    TrackId,
+)
 from OTAnalytics.plugin_parser.otvision_parser import OtsectionParser, OttrkParser
 
 T = TypeVar("T")
 YieldFixture = Generator[T, None, None]
+
+
+@dataclass
+class TrackBuilder:
+    track_id: int = 1
+    track_class: str = "car"
+    detection_class: str = "car"
+    confidence: float = 0.5
+    x: float = 0
+    y: float = 0
+    w: float = 10
+    h: float = 10
+    frame: int = 1
+    occurrence_year: int = 2022
+    occurrence_month: int = 1
+    occurrence_day: int = 1
+    occurrence_hour: int = 0
+    occurrence_minute: int = 0
+    occurrence_second: int = 0
+    occurrence_microsecond: int = 0
+    input_file_path: str = "path/to/myhostname_file.otdet"
+    interpolated_detection: bool = False
+
+    def __post_init__(self) -> None:
+        self._detections: list[Detection] = []
+
+    def build_track(self) -> Track:
+        return Track(TrackId(self.track_id), self.track_class, self._detections)
+
+    def build_detections(self) -> list[Detection]:
+        return self._detections
+
+    def append_detection(self) -> None:
+        self._detections.append(
+            Detection(
+                classification=self.detection_class,
+                confidence=self.confidence,
+                x=self.x,
+                y=self.y,
+                w=self.w,
+                h=self.h,
+                frame=self.frame,
+                occurrence=datetime(
+                    self.occurrence_year,
+                    self.occurrence_month,
+                    self.occurrence_day,
+                    self.occurrence_hour,
+                    self.occurrence_minute,
+                    self.occurrence_second,
+                    self.occurrence_microsecond,
+                ),
+                input_file_path=Path(self.input_file_path),
+                interpolated_detection=self.interpolated_detection,
+                track_id=TrackId(self.track_id),
+            )
+        )
+
+    def add_track_id(self, id: int) -> None:
+        self.track_id = id
+
+    def add_detection_class(self, classification: str) -> None:
+        self.detection_class = classification
+
+    def add_confidence(self, confidence: float) -> None:
+        self.confidence = confidence
+
+    def add_frame(self, frame: int) -> None:
+        self.frame = frame
+
+    def add_track_class(self, classification: str) -> None:
+        self.track_class = classification
+
+    def add_occurrence(
+        self,
+        year: int,
+        month: int,
+        day: int,
+        hour: int,
+        minute: int,
+        second: int,
+        microsecond: int,
+    ) -> None:
+        self.occurrence_year = year
+        self.occurrence_month = month
+        self.occurrence_day = day
+        self.occurrence_hour = hour
+        self.occurrence_minute = minute
+        self.occurrence_second = second
+        self.occurrence_microsecond = microsecond
+
+    def add_microsecond(self, microsecond: int) -> None:
+        self.occurrence_microsecond = microsecond
+
+    def add_xy_bbox(self, x: float, y: float) -> None:
+        self.x = x
+        self.y = y
+
+
+@dataclass
+class EventBuilder:
+    road_user_id: int = 1
+    road_user_type: str = "car"
+    hostname: str = "myhostname"
+    occurrence_year: int = 2022
+    occurrence_month: int = 1
+    occurrence_day: int = 1
+    occurrence_hour: int = 0
+    occurrence_minute: int = 0
+    occurrence_second: int = 0
+    occurrence_microsecond: int = 0
+    frame_number: int = 1
+    section_id: str = "N"
+    event_coordinate_x: float = 0.0
+    event_coordinate_y: float = 0.0
+    event_type: str = "section-enter"
+    direction_vector_x: float = 0.0
+    direction_vector_y: float = 0.0
+    video_name: str = "myhostname_file.otdet"
+
+    def __post_init__(self) -> None:
+        self._events: list[Event] = []
+
+    def build_events(self) -> list[Event]:
+        return self._events
+
+    def build_section_event(self) -> Event:
+        return Event(
+            road_user_id=self.road_user_id,
+            road_user_type=self.road_user_type,
+            hostname=self.hostname,
+            occurrence=datetime(
+                self.occurrence_year,
+                self.occurrence_month,
+                self.occurrence_day,
+                self.occurrence_hour,
+                self.occurrence_minute,
+                self.occurrence_second,
+                self.occurrence_microsecond,
+            ),
+            frame_number=self.frame_number,
+            section_id=self.section_id,
+            event_coordinate=ImageCoordinate(
+                self.event_coordinate_x, self.event_coordinate_y
+            ),
+            event_type=EventType.parse(self.event_type),
+            direction_vector=DirectionVector2D(
+                self.direction_vector_x, self.direction_vector_y
+            ),
+            video_name=self.video_name,
+        )
+
+    def append_section_event(self) -> None:
+        self._events.append(self.build_section_event())
+
+    def add_microsecond(self, microsecond: int) -> None:
+        self.occurrence_microsecond = microsecond
+
+    def add_frame_number(self, frame_number: int) -> None:
+        self.frame_number = frame_number
+
+    def add_event_type(self, event_type: str) -> None:
+        self.event_type = event_type
+
+    def add_event_coordinate(self, x: float, y: float) -> None:
+        self.event_coordinate_x = x
+        self.event_coordinate_y = y
 
 
 @pytest.fixture(scope="module")
@@ -53,3 +228,13 @@ def tracks(ottrk_path: Path) -> list[Track]:
 def sections(otsection_file: Path) -> list[Section]:
     otsection_parser = OtsectionParser()
     return otsection_parser.parse(otsection_file)
+
+
+@pytest.fixture
+def track_builder() -> TrackBuilder:
+    return TrackBuilder()
+
+
+@pytest.fixture
+def event_builder() -> EventBuilder:
+    return EventBuilder()
