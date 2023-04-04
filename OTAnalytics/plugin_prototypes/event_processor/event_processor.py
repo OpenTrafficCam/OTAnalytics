716214a659f6b7f30bf685f8051eba5c6a30a474
Sebastian Buck <38660441+frunika@users.noreply.github.com>
2023-04-04 19:08:22 +0200

    move parser for plugin to plugin

--- OTAnalytics/plugin_prototypes/event_processor/event_processor.py
+++ OTAnalytics/plugin_prototypes/event_processor/event_processor.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 
-from OTAnalytics.plugin_parser.otanalytics_parser import (
+from OTAnalytics.plugin_prototypes.event_processor.otanalytics_parser import (
     JsonParser,
     PandasDataFrameParser,
 )
