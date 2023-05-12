8dd32fa12d3d7a6ab885a56bddd47180449406f6
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2023-05-12 14:15:16 +0200

    Fix mypy error

--- OTAnalytics/plugin_prototypes/template/greetings.py
+++ OTAnalytics/plugin_prototypes/template/greetings.py
@@ -1,9 +1,8 @@
 from typing import Iterable
 
+from OTAnalytics.application.application import OTAnalyticsApplication
 from OTAnalytics.domain.event import Event
 from OTAnalytics.plugin_ui.main_application import ApplicationStarter
-
-otanalytics = ApplicationStarter().build_application()
 
 
 class EnglishGreeter:
@@ -20,3 +19,25 @@ class EventPrinter:
     def print_all_events(self, events: Iterable[Event]) -> None:
         for event in events:
             print(event)
+
+
+def build_otanalytics_application() -> OTAnalyticsApplication:
+    starter = ApplicationStarter()
+
+    datastore = starter._create_datastore()
+    track_state = starter._create_track_state()
+    track_view_state = starter._create_track_view_state(datastore)
+    section_state = starter._create_section_state()
+    intersect = starter._create_intersect()
+    scene_event_detection = starter._create_scene_event_detection()
+    return OTAnalyticsApplication(
+        datastore=datastore,
+        track_state=track_state,
+        track_view_state=track_view_state,
+        section_state=section_state,
+        intersect=intersect,
+        scene_event_detection=scene_event_detection,
+    )
+
+
+otanalytics = build_otanalytics_application()
