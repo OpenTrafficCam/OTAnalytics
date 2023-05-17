667834c94ff1b865210ea3fc56e4c88fb2f4e8d0
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2023-05-17 16:52:30 +0200

    Add TkEvents class holding tkinter event strings

--- OTAnalytics/plugin_ui/customtkinter_gui/constants.py
+++ OTAnalytics/plugin_ui/customtkinter_gui/constants.py
@@ -1,3 +1,8 @@
+from dataclasses import dataclass
+from typing import ClassVar
+
+from OTAnalytics.application.config import ON_MAC
+
 PADX = 10
 PADY = 5
 STICKY = "NESW"
@@ -16,3 +21,41 @@ LEFT_KEY = "left"
 RIGHT_KEY = "right"
 PLUS_KEYS = "+"
 MINUS_KEYS = "-"
+
+
+@dataclass
+class TkEvents:
+    """
+    Class holding tkinter events as class properties.
+    The strings behind some of the events depend on the platform the software is
+    running on (Linux, Mac, or Windows).
+    """
+
+    RIGHT_BUTTON_DOWN: ClassVar[str] = "<Button-2>" if ON_MAC else "<Button-3>"
+    RIGHT_BUTTON_UP: ClassVar[str] = (
+        "<ButtonRelease-2>" if ON_MAC else "<ButtonRelease-3>"
+    )
+    MIDDLE_BUTTON_DOWN: ClassVar[str] = "<Button-3>" if ON_MAC else "<Button-2>"
+    MIDDLE_BUTTON_UP: ClassVar[str] = (
+        "<ButtonRelease-3>" if ON_MAC else "<ButtonRelease-2>"
+    )
+    LEFT_BUTTON_DOWN: ClassVar[str] = "<Button-1>"
+    LEFT_BUTTON_UP: ClassVar[str] = "<ButtonRelease-1>"
+    MOUSE_MOTION: ClassVar[str] = "<Motion>"
+    MOUSE_MOTION_WHILE_LEFT_BUTTON_DOWN: ClassVar[str] = "<B1-Motion>"
+    MOUSE_ENTERS_WIDGET: ClassVar[str] = "<Enter>"
+    MOUSE_LEAVES_WIDGET: ClassVar[str] = "<Leave>"
+    PLUS_KEY: ClassVar[str] = "+"
+    KEYPAD_PLUS_KEY: ClassVar[str] = "<KP_Add>"
+    LEFT_ARROW_KEY: ClassVar[str] = "<Left>"
+    RIGHT_ARROW_KEY: ClassVar[str] = "<Right>"
+    UP_ARROW_KEY: ClassVar[str] = "<Up>"
+    DOWN_ARROW_KEY: ClassVar[str] = "<Down>"
+    RETURN_KEY: ClassVar[str] = "<Return>"
+    KEYPAD_RETURN_KEY: ClassVar[str] = "<KP_Enter>"
+    DELETE_KEY: ClassVar[str] = "<Delete>"
+    BACKSPACE_KEY: ClassVar[str] = "<BackSpace>"
+    ESCAPE_KEY: ClassVar[str] = "<Escape>"
+
+
+tk_events = TkEvents()
