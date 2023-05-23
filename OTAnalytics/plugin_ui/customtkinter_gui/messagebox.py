a0a2269589fe553c34010182ea6a78333410ad22
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2023-05-23 18:09:43 +0200

    Bring messagebox to the very top persistently

--- OTAnalytics/plugin_ui/customtkinter_gui/messagebox.py
+++ OTAnalytics/plugin_ui/customtkinter_gui/messagebox.py
@@ -34,6 +34,7 @@ class InfoBox(CTkToplevel):
         self.button_ok.grid(row=1, column=0, padx=PADX, pady=PADY, sticky=STICKY)
 
     def _set_focus(self) -> None:
+        self.attributes("-topmost", 1)
         self.after(0, lambda: self.button_ok.focus_set())
 
     def _set_close_on_return_key(self) -> None:
