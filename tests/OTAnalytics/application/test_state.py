67585fc4abe219563484fd1344ef3936b5680293
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2023-06-19 12:42:04 +0200

    Use max class of tracks instead of detection classes for classification filter

--- tests/OTAnalytics/application/test_state.py
+++ tests/OTAnalytics/application/test_state.py
@@ -247,20 +247,13 @@ class TestTracksMetadata:
 
         tracks_metadata._update_classifications([track.id])
 
-        assert tracks_metadata.classifications == {"car", "truck"}
+        assert tracks_metadata.classifications == {"car"}
         mock_track_repository.get_for.assert_any_call(track.id)
         assert mock_track_repository.get_for.call_count == 1
 
         track.detections[0].classification = "bicycle"
         tracks_metadata._update_classifications([track.id])
 
-        assert tracks_metadata.classifications == {"car", "truck", "bicycle"}
+        assert tracks_metadata.classifications == {"car"}
         mock_track_repository.get_for.assert_any_call(track.id)
         assert mock_track_repository.get_for.call_count == 2
-
-        track.detections[0].classification = "car"
-        tracks_metadata._update_classifications([track.id])
-
-        assert tracks_metadata.classifications == {"car", "truck", "bicycle"}
-        mock_track_repository.get_for.assert_any_call(track.id)
-        assert mock_track_repository.get_for.call_count == 3
