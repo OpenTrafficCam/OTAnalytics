19380938ef5e0d46265ce4ba6c255b470019114b
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2023-06-22 19:05:09 +0200

    Add helper to check if coordinate is on widget

--- OTAnalytics/plugin_ui/customtkinter_gui/helpers.py
+++ OTAnalytics/plugin_ui/customtkinter_gui/helpers.py
@@ -27,3 +27,24 @@ def get_widget_position(widget: Widget) -> tuple[int, int]:
     x = widget.winfo_rootx()
     y = widget.winfo_rooty()
     return x, y
+
+
+def coordinate_is_on_widget(coordinate: tuple[int, int], widget: Widget) -> bool:
+    """Returns True, if the coordinate is within the boundaries of an existing
+        tkinter (or customtkinter) widget.
+
+    Args:
+        coordinate (tuple[int, int]): Coordinate
+        widget (Widget): Tkinter or customtkinter Widget
+
+    Returns:
+        bool: True if the coordinate is within tho boundaries of the widget,
+            false otherwise.
+    """
+    widget_width, widget_heigt = get_widget_size(widget)
+    x, y = coordinate
+    left_in = x >= 0
+    right_in = x <= widget_width
+    top_in = y >= 0
+    bottom_in = y <= widget_heigt
+    return False not in [left_in, right_in, top_in, bottom_in]
