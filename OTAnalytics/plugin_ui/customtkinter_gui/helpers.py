4d7c6883886ec394fd5606425ae74249d61d3d07
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2023-06-22 19:04:45 +0200

    Add helper to get widget size

--- OTAnalytics/plugin_ui/customtkinter_gui/helpers.py
+++ OTAnalytics/plugin_ui/customtkinter_gui/helpers.py
@@ -1,6 +1,20 @@
 from tkinter import Widget
 
 
+def get_widget_size(widget: Widget) -> tuple[int, int]:
+    """Returns the current size of a tkinter (or customtkinter) widget.
+
+    Args:
+        widget (Widget): tkinter (or customtkinter) widget
+
+    Returns:
+        tuple[int, int]: Tuple of width and height of the widget
+    """
+    width = widget.winfo_width()
+    height = widget.winfo_height()
+    return width, height
+
+
 def get_widget_position(widget: Widget) -> tuple[int, int]:
     """Returns the current position of a tkinter (or customtkinter) widget.
 
