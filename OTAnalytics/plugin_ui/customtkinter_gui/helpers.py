8414db9f3a6eee48e38bc898738d28516f1ea2c4
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2023-06-23 10:07:19 +0200

    Hint that coordinates should be relative to widget

--- OTAnalytics/plugin_ui/customtkinter_gui/helpers.py
+++ OTAnalytics/plugin_ui/customtkinter_gui/helpers.py
@@ -29,12 +29,18 @@ def get_widget_position(widget: Widget) -> tuple[int, int]:
     return x, y
 
 
-def coordinate_is_on_widget(coordinate: tuple[int, int], widget: Widget) -> bool:
+def coordinate_is_on_widget(
+    coordinate_relative_to_widget: tuple[int, int], widget: Widget
+) -> bool:
     """Returns True, if the coordinate is within the boundaries of an existing
         tkinter (or customtkinter) widget.
 
     Args:
-        coordinate (tuple[int, int]): Coordinate
+        coordinate (tuple[int, int]): Coordinate relative to the widget position.
+            E.g. for the tkinter/customtkinter canvas, absolute coordinates have to be
+            transformed to relative coordinates using Canvas.canvasx(absolute_x) as well
+            as Canvas.canvasy(absolute_y).
+
         widget (Widget): Tkinter or customtkinter Widget
 
     Returns:
@@ -42,7 +48,7 @@ def coordinate_is_on_widget(coordinate: tuple[int, int], widget: Widget) -> bool
             false otherwise.
     """
     widget_width, widget_heigt = get_widget_size(widget)
-    x, y = coordinate
+    x, y = coordinate_relative_to_widget
     left_in = x >= 0
     right_in = x <= widget_width
     top_in = y >= 0
