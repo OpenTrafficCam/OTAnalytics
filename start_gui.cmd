ac9e1cb2f7bd923d9101d771a78be4c4c69aef7e
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2023-06-29 13:08:41 +0200

    Make start up script use python 3.11

--- start_gui.cmd
+++ start_gui.cmd
@@ -4,7 +4,7 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`python --version`) DO SET PYTHON_VERSION=%%F
 
 echo %PYTHON_VERSION%
 if "x%PYTHON_VERSION:3.10=%"=="x%PYTHON_VERSION%" (
-    echo "Python Version 3.10 is not installed in environment." & cmd /K & exit
+    echo "Python Version 3.11 is not installed in environment." & cmd /K & exit
 )
 
 call venv\Scripts\activate
