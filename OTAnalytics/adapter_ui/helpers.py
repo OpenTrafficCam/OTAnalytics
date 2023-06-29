36499caa1bf68ce76a3a45a905b6befcca8aef98
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2023-06-29 11:08:10 +0200

    Revert "Let derived classes define default parameter value"
    This reverts commit d7538cd47d6c1e6b01131e1fbbb748adac1779c4.

--- OTAnalytics/adapter_ui/helpers.py
+++ OTAnalytics/adapter_ui/helpers.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class WidgetPositionProvider(ABC):
     @abstractmethod
-    def get_position(self, offset: tuple[float, float]) -> tuple[int, int]:
+    def get_position(self, offset: tuple[float, float] = (0.5, 0.5)) -> tuple[int, int]:
         raise NotImplementedError
