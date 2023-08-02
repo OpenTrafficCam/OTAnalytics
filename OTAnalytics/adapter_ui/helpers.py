5b30ff047b4fca34ddd9cec8ceae61025935e049
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2023-08-02 21:36:45 +0200

    Check allowed extensions before setting default

--- OTAnalytics/adapter_ui/helpers.py
+++ OTAnalytics/adapter_ui/helpers.py
@@ -11,21 +11,26 @@ def remove_wildcard_from(file_extension: str) -> str:
     return file_extension[1:] if file_extension.startswith("*") else file_extension
 
 
-def ensure_file_extension_is_present(file: str, defaultextension: str) -> str:
+def ensure_file_extension_is_present(
+    file: str, allowed_extensions: list[str], defaultextension: str
+) -> str:
     """
     Ensure that the file contains a file extension. If no extension is appended, the
     defaultextension will be used.
 
     Args:
         file (str): file to ensure it has a file extension
+        allowed_extensions (list[str]): extensions that are allowed
         defaultextension (str): default extension to be added if extension is missing
 
     Returns:
         Path: path object with file extension
     """
     file_extension = remove_wildcard_from(defaultextension)
-    if file.endswith(file_extension):
-        return file
+    allowed_extensions = [remove_wildcard_from(ext) for ext in allowed_extensions]
+    for allowed_extension in allowed_extensions:
+        if file.endswith(allowed_extension):
+            return file
     if file_extension.startswith("."):
         return file + file_extension
     return f"{file}.{file_extension}"
