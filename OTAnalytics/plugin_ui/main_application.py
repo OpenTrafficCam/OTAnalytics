a570ef12b7e5e9986b0699fa9572c7f746dc2acb
Lars Briem <lars.briem@platomo.de>
2023-08-02 14:47:15 +0200

    Filter existing flows during generation

--- OTAnalytics/plugin_ui/main_application.py
+++ OTAnalytics/plugin_ui/main_application.py
@@ -28,6 +28,7 @@ from OTAnalytics.application.eventlist import SceneActionDetector
 from OTAnalytics.application.generate_flows import (
     ArrowFlowNameGenerator,
     CrossProductFlowGenerator,
+    FilterExisting,
     FilterSameSection,
     FlowIdGenerator,
     GenerateFlows,
@@ -618,7 +619,7 @@ class ApplicationStarter:
         flow_generator = CrossProductFlowGenerator(
             id_generator=id_generator,
             name_generator=name_generator,
-            predicate=FilterSameSection(),
+            predicate=FilterSameSection().andThen(FilterExisting(flow_repository)),
         )
         return GenerateFlows(
             section_repository=section_repository,
