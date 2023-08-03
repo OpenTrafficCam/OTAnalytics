dcbc97728e1b54138f9c735d902a47b971fc652f
Lars Briem <lars.briem@platomo.de>
2023-08-03 10:09:27 +0200

    Remove duplication

--- OTAnalytics/adapter_ui/abstract_frame.py
+++ OTAnalytics/adapter_ui/abstract_frame.py
@@ -8,18 +8,17 @@ STATE_NORMAL = "normal"
 
 class AbstractFrame(ABC):
     def set_enabled_add_buttons(self, enabled: bool) -> None:
-        new_state = STATE_NORMAL if enabled else STATE_DISABLED
-        for button in self.add_buttons():
-            button.configure(state=new_state)
+        self._set_enabled_buttons(self.add_buttons(), enabled)
 
     def set_enabled_change_single_item_buttons(self, enabled: bool) -> None:
-        new_state = STATE_NORMAL if enabled else STATE_DISABLED
-        for button in self.single_item_buttons():
-            button.configure(state=new_state)
+        self._set_enabled_buttons(self.single_item_buttons(), enabled)
 
     def set_enabled_change_multiple_items_buttons(self, enabled: bool) -> None:
+        self._set_enabled_buttons(self.multiple_items_buttons(), enabled)
+
+    def _set_enabled_buttons(self, buttons: list[CTkButton], enabled: bool) -> None:
         new_state = STATE_NORMAL if enabled else STATE_DISABLED
-        for button in self.multiple_items_buttons():
+        for button in buttons:
             button.configure(state=new_state)
 
     @abstractmethod
