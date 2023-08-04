05ed61118204f3aa5c5ec7dd9cf5895513542107
Lars Briem <lars.briem@platomo.de>
2023-08-04 09:37:57 +0200

    Clean up view model interface

--- OTAnalytics/adapter_ui/view_model.py
+++ OTAnalytics/adapter_ui/view_model.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
-from pathlib import Path
 from typing import Callable, Iterable, Optional
 
 from OTAnalytics.adapter_ui.abstract_canvas import AbstractCanvas
@@ -121,15 +120,7 @@ class ViewModel(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def _load_otflow(self, otflow_file: Path) -> None:
-        raise NotImplementedError
-
-    @abstractmethod
     def save_configuration(self) -> None:
-        raise NotImplementedError
-
-    @abstractmethod
-    def _save_otflow(self, otflow_file: Path) -> None:
         raise NotImplementedError
 
     @abstractmethod
