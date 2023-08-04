50471e656563b9c3c4a978aba89f03014c0dd493
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2023-08-04 21:34:45 +0200

    Add helper to list all ExceptionGroup messages

---
+++ OTAnalytics/application/helpers.py
@@ -0,0 +1,44 @@
+import re
+
+
+def get_all_messages_from_exception_group(
+    exception_group: BaseExceptionGroup,
+) -> list[str]:
+    """Returns a list of messages of the ExceptionGroup itself and all of its
+    sub-exceptions.
+    If a sub-exception is an ExceptionGroup itself, the list will also contain its
+    message and the messages of its sub-exceptions and so on...
+
+    Args:
+        exception_group (BaseExceptionGroup): The ExceptionGroup instance
+        messages (list[str] | None, optional): An initial list of messages.
+            Is only there for matters of recursion. Defaults to None.
+
+    Returns:
+        list[str]: List of all messages of the ExceptionGroup and it leafs
+    """
+
+    def _get_all_messages_from_exception_group(
+        exception_group: BaseExceptionGroup,
+        messages: list[str] | None = None,
+    ) -> list[str]:
+        if messages is None:
+            messages = []
+        PATTERN = re.compile(r" .((\d+)\s*sub-exception.*.)")
+        if match := re.search(PATTERN, str(exception_group)):
+            exception_group_message = str(exception_group).replace(match[0], "")
+        else:
+            exception_group_message = str(exception_group)
+        messages.append(exception_group_message)
+        for sub_exception in exception_group.exceptions:
+            if isinstance(sub_exception, BaseExceptionGroup):
+                messages = _get_all_messages_from_exception_group(
+                    sub_exception, messages
+                )
+            else:
+                message = str(sub_exception)
+                message = re.sub("'", "", message)
+                messages.append(message)
+        return messages
+
+    return _get_all_messages_from_exception_group(exception_group, None)
