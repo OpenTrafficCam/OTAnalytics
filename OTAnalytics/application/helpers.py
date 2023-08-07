03637a5e6cfcef98aab009ea4aecde15031ab45b
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2023-08-07 18:29:10 +0200

    Simplify code of getting all messages from exception group

--- OTAnalytics/application/helpers.py
+++ OTAnalytics/application/helpers.py
@@ -1,5 +1,7 @@
 import re
 
+PATTERN = re.compile(r" .((\d+)\s*sub-exception.*.)")
+
 
 def get_all_messages_from_exception_group(
     exception_group: BaseExceptionGroup,
@@ -17,25 +19,13 @@ def get_all_messages_from_exception_group(
         list[str]: List of all messages of the ExceptionGroup and all of the Exceptions
         and ExceptionGroups it contains
     """
+    messages = []
 
-    def _get_all_messages_from_exception_group(
-        exception_group: BaseExceptionGroup,
-    ) -> list[str]:
-        messages = []
-
-        PATTERN = re.compile(r" .((\d+)\s*sub-exception.*.)")
-        if match := re.search(PATTERN, str(exception_group)):
-            messages.append(str(exception_group).replace(match[0], ""))
-        else:
-            messages.append(str(exception_group))
-
+    if isinstance(exception_group, BaseExceptionGroup):
+        messages.append(re.sub(PATTERN, "", str(exception_group)))
         for sub_exception in exception_group.exceptions:
-            if isinstance(sub_exception, BaseExceptionGroup):
-                messages.extend(_get_all_messages_from_exception_group(sub_exception))
-            else:
-                message = str(sub_exception)
-                message = re.sub("'", "", message)
-                messages.append(message)
-        return messages
+            messages.extend(get_all_messages_from_exception_group(sub_exception))
+    else:
+        messages.append(str(exception_group).replace("'", ""))
 
-    return _get_all_messages_from_exception_group(exception_group)
+    return messages
