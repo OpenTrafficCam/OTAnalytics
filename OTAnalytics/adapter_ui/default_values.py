30864a1fe2cb493193e683f8745432b61c4b111c
Lars Briem <lars.briem@platomo.de>
2023-08-10 10:51:00 +0200

    Clean up branch and move maximum number of frames into default values

--- OTAnalytics/adapter_ui/default_values.py
+++ OTAnalytics/adapter_ui/default_values.py
@@ -4,3 +4,5 @@ RELATIVE_SECTION_OFFSET: RelativeOffsetCoordinate = RelativeOffsetCoordinate(0.5
 DATE_FORMAT: str = r"%Y-%m-%d"
 DATETIME_FORMAT: str = r"%Y-%m-%d %H:%M:%S"
 DATE_FORMAT_PLACEHOLDER = "yyyy-mm-dd"
+
+TRACK_LENGTH_LIMIT = 1200
