d1e9ef53b867fa4240fd506426165d9493b34a10
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2023-08-17 18:27:10 +0200

    Fix dict order problem in test on windows runner

--- tests/OTAnalytics/application/analysis/test_traffic_counting.py
+++ tests/OTAnalytics/application/analysis/test_traffic_counting.py
@@ -123,7 +123,7 @@ class TestFillEmptyCount:
 
         actual = count.to_dict()
 
-        assert actual == filled_dict
+        assert dict(actual) == dict(filled_dict)
         other.to_dict.assert_called_once()
 
 
