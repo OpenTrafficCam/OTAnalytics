43c9d68678226e81ff6c35c098bc457442e7250d
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2024-03-13 15:15:59 +0100

    Create use case to add videos to VideoRepository

--- OTAnalytics/application/use_cases/video_repository.py
+++ OTAnalytics/application/use_cases/video_repository.py
@@ -1,4 +1,6 @@
-from OTAnalytics.domain.video import VideoRepository
+from typing import Iterable
+
+from OTAnalytics.domain.video import Video, VideoRepository
 
 
 class ClearAllVideos:
@@ -14,3 +16,12 @@ class ClearAllVideos:
     def __call__(self) -> None:
         """Clear the video repository."""
         self._video_repository.clear()
+
+
+class AddAllVideos:
+
+    def __init__(self, video_repository: VideoRepository) -> None:
+        self._video_repository = video_repository
+
+    def add(self, videos: Iterable[Video]) -> None:
+        self._video_repository.add_all(videos)
--- tests/OTAnalytics/application/use_cases/test_video_repository.py
+++ tests/OTAnalytics/application/use_cases/test_video_repository.py
@@ -1,6 +1,9 @@
 from unittest.mock import Mock
 
-from OTAnalytics.application.use_cases.video_repository import ClearAllVideos
+from OTAnalytics.application.use_cases.video_repository import (
+    AddAllVideos,
+    ClearAllVideos,
+)
 from OTAnalytics.domain.video import VideoRepository
 
 
@@ -10,3 +13,13 @@ class TestClearAllTrackToVideos:
         clear_all_videos = ClearAllVideos(repository)
         clear_all_videos()
         repository.clear.assert_called_once()
+
+
+class TestAddAllVideos:
+    def test_add(self) -> None:
+        repository = Mock(spec=VideoRepository)
+        video = Mock()
+
+        add_all = AddAllVideos(repository)
+        add_all.add([video])
+        repository.add_all.assert_called_once_with([video])
