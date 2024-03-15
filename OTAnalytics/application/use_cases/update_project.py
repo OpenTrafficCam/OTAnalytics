b75674c157747851bec846d9bef44b0fb225c93c
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2024-03-15 16:12:48 +0100

    Make `ProjectUpdater` observable in order to change the quick save button on `Project` changes

--- OTAnalytics/application/use_cases/update_project.py
+++ OTAnalytics/application/use_cases/update_project.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from OTAnalytics.application.datastore import Datastore
 from OTAnalytics.application.project import Project
+from OTAnalytics.domain.observer import OBSERVER, Subject
 
 
 class ProjectUpdater:
@@ -10,14 +11,24 @@ class ProjectUpdater:
 
     def __init__(self, datastore: Datastore) -> None:
         self._datastore = datastore
+        self._subject = Subject[Project]()
 
     def __call__(self, name: str, start_date: Optional[datetime]) -> None:
-        self._datastore.project = Project(name=name, start_date=start_date)
+        project = Project(name=name, start_date=start_date)
+        self._datastore.project = project
+        self._subject.notify(project)
 
     def update_name(self, name: str) -> None:
         old_project = self._datastore.project
-        self._datastore.project = Project(name, old_project.start_date)
+        new_project = Project(name, old_project.start_date)
+        self._datastore.project = new_project
+        self._subject.notify(new_project)
 
     def update_start_date(self, start_date: Optional[datetime]) -> None:
         old_project = self._datastore.project
-        self._datastore.project = Project(old_project.name, start_date)
+        new_project = Project(old_project.name, start_date)
+        self._datastore.project = new_project
+        self._subject.notify(new_project)
+
+    def register(self, observer: OBSERVER[Project]) -> None:
+        self._subject.register(observer)
--- tests/OTAnalytics/application/use_cases/test_update_project.py
+++ tests/OTAnalytics/application/use_cases/test_update_project.py
@@ -25,20 +25,35 @@ class TestUpdateProject:
         new_project_name = "My New Project"
         new_project_start_date = datetime(2000, 2, 2, 13)
         project_updater = ProjectUpdater(datastore)
-        project_updater(new_project_name, new_project_start_date)
+        observer = Mock()
+        project_updater.register(observer)
 
-        assert datastore.project == Project(new_project_name, new_project_start_date)
+        project_updater(new_project_name, new_project_start_date)
+        expected_project = Project(new_project_name, new_project_start_date)
+
+        assert datastore.project == expected_project
+        observer.assert_called_once_with(expected_project)
 
     def test_update_name(self, datastore: Mock, my_project: Project) -> None:
         new_project_name = "My New Project"
         project_updater = ProjectUpdater(datastore)
-        project_updater.update_name(new_project_name)
+        observer = Mock()
+        project_updater.register(observer)
 
-        assert datastore.project == Project(new_project_name, my_project.start_date)
+        project_updater.update_name(new_project_name)
+        expected_project = Project(new_project_name, my_project.start_date)
+
+        assert datastore.project == expected_project
+        observer.assert_called_once_with(expected_project)
 
     def test_update_start_date(self, datastore: Mock, my_project: Project) -> None:
         new_project_start_date = datetime(2000, 2, 2, 13)
         project_updater = ProjectUpdater(datastore)
-        project_updater.update_start_date(new_project_start_date)
+        observer = Mock()
+        project_updater.register(observer)
 
-        assert datastore.project == Project(my_project.name, new_project_start_date)
+        project_updater.update_start_date(new_project_start_date)
+        expected_project = Project(my_project.name, new_project_start_date)
+
+        assert datastore.project == expected_project
+        observer.assert_called_once_with(expected_project)
