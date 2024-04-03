8d0b72f373ad694cb34e8c69b341abe5709adb4e
Lars Briem <lars.briem@platomo.de>
2024-04-03 14:51:00 +0200

    Ensure only configured exports are processed

--- tests/OTAnalytics/application/use_cases/test_track_export.py
+++ tests/OTAnalytics/application/use_cases/test_track_export.py
@@ -10,6 +10,7 @@ from OTAnalytics.application.use_cases.track_export import (
 
 
 class TestMultiExportTracks:
+
     def test_calls_all_exporter(self) -> None:
         specification = TrackExportSpecification(
             save_path=Path("/path/to/export"),
@@ -25,4 +26,21 @@ class TestMultiExportTracks:
         multi_exporter.export(specification)
 
         exporter_1.export.assert_called_once_with(specification)
+        exporter_2.export.assert_called_once_with(specification)
+
+    def test_calls_all_configured_exporters(self) -> None:
+        specification = TrackExportSpecification(
+            save_path=Path("/path/to/export"),
+            export_format=[TrackFileFormat.OTTRK],
+        )
+        exporter_1 = Mock(spec=ExportTracks)
+        exporter_2 = Mock(spec=ExportTracks)
+
+        multi_exporter = MultiExportTracks(
+            {TrackFileFormat.CSV: exporter_1, TrackFileFormat.OTTRK: exporter_2}
+        )
+
+        multi_exporter.export(specification)
+
+        exporter_1.export.assert_not_called()
         exporter_2.export.assert_called_once_with(specification)
