9095975c4fdf1074f7a1c6e82f6a1142a8b33066
Lars Briem <lars.briem@platomo.de>
2024-05-07 16:19:15 +0200

    Use actual fps to calculate frame number if available

--- OTAnalytics/domain/video.py
+++ OTAnalytics/domain/video.py
@@ -112,8 +112,8 @@ class VideoMetadata:
 
     @property
     def fps(self) -> float:
-        # if self.actual_fps:
-        #     return self.actual_fps
+        if self.actual_fps:
+            return self.actual_fps
         return self.recorded_fps
 
     def to_dict(self) -> dict:
@@ -159,10 +159,7 @@ class SimpleVideo(Video):
     @property
     def fps(self) -> float:
         return (
-            # self.metadata.actual_fps
-            # if self.metadata and self.metadata.actual_fps
-            # else
-            self.video_reader.get_fps(self.path)
+            self.metadata.fps if self.metadata else self.video_reader.get_fps(self.path)
         )
 
     def __post_init__(self) -> None:
