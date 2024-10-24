bb8de37af68c0329e11c4a186b30447cca9054bd
Lars Briem <lars.briem@platomo.de>
2024-10-24 09:30:39 +0200

    Reintegrate tools into dev requirements

--- install_dev.cmd
+++ install_dev.cmd
@@ -1,7 +1,7 @@
 echo Install OTAnalytics development environment.
 call install.cmd
 call venv\Scripts\activate
-pip install -r requirements-tools.txt --no-cache-dir%
+pip install -r requirements-dev.txt --no-cache-dir%
 pip install -e .
 pre-commit install --install-hooks
 deactivate
