ec6e1c85a797483474e8091df1dafd18e6f3bc6e
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2024-11-20 10:13:20 +0100

    Rename method to initialize flow track counts
    Renamed the method `_tracks_assigned_to_flows` to `_initialize_flow_track_counts` to better reflect its function. This change improves readability and makes the code more intuitive for future maintenance.

--- OTAnalytics/application/use_cases/flow_statistics.py
+++ OTAnalytics/application/use_cases/flow_statistics.py
@@ -12,13 +12,13 @@ class NumberOfTracksAssignedToEachFlow:
         self._flow_repository = flow_repository
 
     def get(self) -> dict[FlowId, int]:
-        result = self._tracks_assigned_to_flows()
+        result = self._initialize_flow_track_counts()
         for road_user_assignment in self._get_assignments.get():
             flow_id = road_user_assignment.assignment.id
             result[flow_id] += 1
         return result
 
-    def _tracks_assigned_to_flows(self) -> dict[FlowId, int]:
+    def _initialize_flow_track_counts(self) -> dict[FlowId, int]:
         flows = {}
         for flow in self._flow_repository.get_all():
             flows[flow.id] = 0
