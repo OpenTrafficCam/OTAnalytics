c3b2031b70587476453d59d8c1874ae9ed3fa0e2
schubsen <uwind@student.kit.edu>
2024-12-18 14:58:52 +0100

    Added unit test with simulaneous events

--- tests/OTAnalytics/application/use_cases/test_track_statistics.py
+++ tests/OTAnalytics/application/use_cases/test_track_statistics.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unittest.mock import Mock
 
 import pytest
@@ -8,6 +9,9 @@ from OTAnalytics.application.use_cases.highlight_intersections import (
 )
 from OTAnalytics.application.use_cases.inside_cutting_section import (
     TrackIdsInsideCuttingSections,
+)
+from OTAnalytics.application.use_cases.number_of_tracks_to_be_validated import (
+    NumberOfTracksToBeValidated,
 )
 from OTAnalytics.application.use_cases.track_repository import GetAllTrackIds
 from OTAnalytics.application.use_cases.track_statistics import CalculateTrackStatistics
@@ -35,6 +39,11 @@ def get_all_track_ids() -> Mock:
 @pytest.fixture
 def track_ids_inside_cutting_sections() -> Mock:
     return Mock(spec=TrackIdsInsideCuttingSections)
+
+
+@pytest.fixture
+def number_of_tracks_to_be_validated() -> Mock:
+    return Mock(spec=NumberOfTracksToBeValidated)
 
 
 @pytest.fixture
@@ -109,3 +118,51 @@ class TestCalculateTrackStatistics:
         )
         given_number_of_tracks_to_be_validated.calculate.assert_called_once()
         assert trackStatistics.number_of_tracks_with_simultaneous_section_events == 0
+
+    def test_get_number_of_tracks_with_simultaneous_events_while_simultaneous_events(
+        self,
+        intersection_all_non_cutting_sections: Mock,
+        assigned_to_all_flows: Mock,
+        get_all_track_ids: Mock,
+        track_ids_inside_cutting_sections: Mock,
+        number_of_tracks_to_be_validated: Mock,
+    ) -> None:
+        event1 = Mock()
+        event1.road_user_id = "1"
+        event1.occurrence = datetime(2024, 12, 18, 9, 0, 17, 4)
+        event2 = Mock()
+        event2.road_user_id = "2"
+        event2.occurrence = datetime(2024, 12, 18, 9, 0, 23, 4)
+        event3 = Mock()
+        event3.road_user_id = "2"
+        event3.occurrence = datetime(2024, 12, 18, 9, 0, 23, 4)
+        event4 = Mock()
+        event4.road_user_id = "2"
+        event4.occurrence = datetime(2024, 12, 18, 10, 0, 21, 0)
+        event5 = Mock()
+        event5.road_user_id = "3"
+        event5.occurrence = datetime(2024, 12, 18, 10, 0, 21, 0)
+
+        get_all_enter_section_events = Mock()
+        get_all_enter_section_events.get.return_value = [
+            event1,
+            event2,
+            event3,
+            event4,
+            event5,
+        ]
+
+        calculator = CalculateTrackStatistics(
+            intersection_all_non_cutting_sections,
+            assigned_to_all_flows,
+            get_all_track_ids,
+            track_ids_inside_cutting_sections,
+            number_of_tracks_to_be_validated,
+            get_all_enter_section_events,
+        )
+
+        simultaneous_event_count = (
+            calculator.get_number_of_tracks_with_simultaneous_events()
+        )
+
+        assert simultaneous_event_count == 1
