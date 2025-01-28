f396430b3f4694c451121c8d736ff78cf6fd2a1f
Lars Briem <lars.briem@platomo.de>
2025-01-28 10:17:29 +0100

    Extend test case

--- tests/OTAnalytics/application/use_cases/test_load_otconfig.py
+++ tests/OTAnalytics/application/use_cases/test_load_otconfig.py
@@ -11,6 +11,8 @@ from OTAnalytics.application.use_cases.load_otconfig import (
 )
 from OTAnalytics.application.use_cases.section_repository import SectionAlreadyExists
 
+REMARK = "my remark"
+
 
 class TestLoadOtconfig:
     @pytest.fixture
@@ -28,6 +30,7 @@ class TestLoadOtconfig:
         otconfig.sections = Mock()
         otconfig.flows = Mock()
         otconfig.analysis = analysis
+        otconfig.remark = REMARK
         return otconfig
 
     def test_load(self, otconfig: OtConfig) -> None:
@@ -74,6 +77,7 @@ class TestLoadOtconfig:
             ConfigurationFile(file, deserialization_result)
         )
         clear_repositories.assert_called_once()
+        remark_repository.add.assert_called_once_with(REMARK)
 
     def test_load_error(self, otconfig: OtConfig) -> None:
         clear_repositories = Mock()
@@ -82,6 +86,7 @@ class TestLoadOtconfig:
 
         add_sections = MagicMock()
         add_sections.add.side_effect = SectionAlreadyExists
+        remark_repository = Mock()
 
         load_otconfig = LoadOtconfig(
             clear_repositories,
@@ -91,7 +96,7 @@ class TestLoadOtconfig:
             add_sections,
             Mock(),
             Mock(),
-            Mock(),
+            remark_repository,
             Mock(),
         )
         observer = Mock()
@@ -103,3 +108,4 @@ class TestLoadOtconfig:
         assert clear_repositories.call_count == 2
         config_parser.parse.assert_called_once_with(file)
         observer.assert_not_called()
+        remark_repository.add.assert_not_called()
