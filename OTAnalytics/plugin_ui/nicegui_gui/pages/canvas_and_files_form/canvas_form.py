0aceb31af764ca8a42664b39be2de658e44228d3
Lars Briem <lars.briem@platomo.de>
2025-02-28 09:29:13 +0100

    Add preview image

--- OTAnalytics/plugin_ui/nicegui_gui/pages/canvas_and_files_form/canvas_form.py
+++ OTAnalytics/plugin_ui/nicegui_gui/pages/canvas_and_files_form/canvas_form.py
@@ -1,29 +1,54 @@
-from typing import Self
+from typing import Optional, Self
 
 from nicegui import ui
+from nicegui.elements.interactive_image import InteractiveImage
+from PIL import Image
 
-from OTAnalytics.adapter_ui.abstract_frame_project import AbstractFrameSvzMetadata
+from OTAnalytics.adapter_ui.abstract_frame_canvas import AbstractFrameCanvas
 from OTAnalytics.adapter_ui.view_model import ViewModel
-from OTAnalytics.application.resources.resource_manager import ResourceManager
+from OTAnalytics.application.resources.resource_manager import (
+    CanvasKeys,
+    ResourceManager,
+)
+from OTAnalytics.domain.track import TrackImage
 
 
-class CanvasForm(AbstractFrameSvzMetadata):
+class CanvasForm(AbstractFrameCanvas):
+
     def __init__(
         self,
-        view_model: ViewModel,
+        viewmodel: ViewModel,
         resource_manager: ResourceManager,
     ) -> None:
-        self._view_model = view_model
+        self._viewmodel = viewmodel
         self._resource_manager = resource_manager
+        self._background_image: Optional[InteractiveImage] = None
+        self._current_image: Optional[Image.Image] = None
 
         self.introduce_to_viewmodel()
+        self.add_preview_image()
 
     def introduce_to_viewmodel(self) -> None:
-        self._view_model.set_svz_metadata_frame(self)
+        self._viewmodel.set_frame_canvas(self)
+
+    def add_preview_image(self) -> None:
+        self._current_image = self._resource_manager.get_image(CanvasKeys.IMAGE_DEFAULT)
 
     def build(self) -> Self:
-        ui.interactive_image("https://picsum.photos/640/360").classes("w-64")
+        self._background_image = ui.interactive_image("").classes("w-64")
+        self._change_image()
         return self
 
-    def update(self, metadata: dict) -> None:
-        pass
+    def update_background(self, image: TrackImage) -> None:
+        self._current_image = image.as_image()
+        self._change_image()
+
+    def _change_image(self) -> None:
+        if self._current_image and self._background_image:
+            self._background_image.set_source(self._current_image)
+        else:
+            self.clear_image()
+
+    def clear_image(self) -> None:
+        if self._background_image:
+            self._background_image.set_source("")
