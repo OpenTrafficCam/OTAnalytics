4b617090016ec372ad0842b1585b2c3ac4e5c3f5
Steven Schlechte <steven.schlechte@gmx.net>
2025-04-07 12:21:47 +0200

    Remove unused ViewModel import from abstract_frame_section.py

--- OTAnalytics/adapter_ui/abstract_frame_section.py
+++ OTAnalytics/adapter_ui/abstract_frame_section.py
@@ -1,7 +1,6 @@
 from abc import abstractmethod
 
 from OTAnalytics.adapter_ui.abstract_frame import AbstractFrame
-from OTAnalytics.adapter_ui.view_model import ViewModel
 from OTAnalytics.domain.geometry import RelativeOffsetCoordinate
 
 
@@ -9,11 +8,11 @@ class AbstractSectionFrame(AbstractFrame):
 
     @abstractmethod
     def configure_section(
-            self,
-            title: str,
-            section_offset: RelativeOffsetCoordinate,
-            initial_position: tuple[int, int],
-            input_values: dict | None,
-            show_offset: bool,
+        self,
+        title: str,
+        section_offset: RelativeOffsetCoordinate,
+        initial_position: tuple[int, int],
+        input_values: dict | None,
+        show_offset: bool,
     ) -> dict:
         raise NotImplementedError
