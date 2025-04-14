7737f60fddba7ac9c4dee69f273ad2376c31a785
Lars Briem <lars.briem@platomo.de>
2025-04-14 12:11:45 +0200

    Rename tabs

--- OTAnalytics/plugin_ui/nicegui_gui/pages/add_track_form/container.py
+++ OTAnalytics/plugin_ui/nicegui_gui/pages/add_track_form/container.py
@@ -33,17 +33,17 @@ class TrackForm:
 
     def build(self) -> None:
         with ui.tabs().classes("w-full") as tabs:
-            one = ui.tab(
+            track_tab = ui.tab(
                 self._resource_manager.get(TrackFormKeys.TAB_ONE),
             )
-            two = ui.tab(
+            video_tab = ui.tab(
                 self._resource_manager.get(
                     TrackFormKeys.TAB_TWO,
                 )
             )
-        with ui.tab_panels(tabs, value=one).classes("w-full"):
-            with ui.tab_panel(one):
+        with ui.tab_panels(tabs, value=track_tab).classes("w-full"):
+            with ui.tab_panel(track_tab):
                 self.add_tracks_form.build()
                 self.offset_slider_form.build()
-            with ui.tab_panel(two):
+            with ui.tab_panel(video_tab):
                 self.add_videos_form.build()
--- OTAnalytics/plugin_ui/nicegui_gui/pages/sections_and_flow_form/container.py
+++ OTAnalytics/plugin_ui/nicegui_gui/pages/sections_and_flow_form/container.py
@@ -28,10 +28,12 @@ class SectionsAndFlowForm:
 
     def build(self) -> None:
         with ui.tabs().classes("w-full") as tabs:
-            one = ui.tab(self._resource_manager.get(FlowAndSectionKeys.TAB_SECTION))
-            two = ui.tab(self._resource_manager.get(FlowAndSectionKeys.TAB_FLOW))
-        with ui.tab_panels(tabs, value=one).classes("w-full"):
-            with ui.tab_panel(one):
+            section_tab = ui.tab(
+                self._resource_manager.get(FlowAndSectionKeys.TAB_SECTION)
+            )
+            flow_tab = ui.tab(self._resource_manager.get(FlowAndSectionKeys.TAB_FLOW))
+        with ui.tab_panels(tabs, value=section_tab).classes("w-full"):
+            with ui.tab_panel(section_tab):
                 self.sections_form.build()
-            with ui.tab_panel(two):
+            with ui.tab_panel(flow_tab):
                 self.flow_form.build()
