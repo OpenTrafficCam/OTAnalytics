9dc65ffdc06faabb8db4e0b1e8079faf8bd3b3d0
Lars Briem <lars.briem@platomo.de>
2025-04-24 15:35:46 +0200

    Add use case to filter all tracks by id and return a dataset with the last 2 detections (last segment)

---
+++ OTAnalytics/application/use_cases/streaming/get_current_frame_track_segments.py
@@ -0,0 +1,37 @@
+from typing import Protocol
+
+from OTAnalytics.application.use_cases.create_intersection_events import GetTracks
+from OTAnalytics.domain.track_dataset import TrackDataset
+from OTAnalytics.plugin_datastore.track_store import (
+    FilterByIdPandasTrackDataset,
+    FilterLastNSegmentsPandasTrackDataset,
+    PandasTrackDataset,
+)
+
+NUMBER_OF_DETECTIONS_TO_FORM_SEGMENT = 2
+
+
+class PandasTrackDatasetFactory(Protocol):
+    def from_dataset(self, other: TrackDataset) -> PandasTrackDataset:
+        raise NotImplementedError
+
+
+class GetCurrentFrameTrackSegments:
+    def __init__(
+        self,
+        get_all_tracks: GetTracks,
+        track_ids: list[str],
+        track_dataset_factory: PandasTrackDatasetFactory,
+    ) -> None:
+        self._get_all_tracks = get_all_tracks
+        self._track_ids = track_ids
+        self._track_dataset_factory = track_dataset_factory
+
+    def as_dataset(self) -> TrackDataset:
+        all_tracks = self._track_dataset_factory.from_dataset(
+            self._get_all_tracks.as_dataset()
+        )
+        other = FilterByIdPandasTrackDataset(all_tracks, track_ids=self._track_ids)
+        return FilterLastNSegmentsPandasTrackDataset(
+            other, NUMBER_OF_DETECTIONS_TO_FORM_SEGMENT
+        )
