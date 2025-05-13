0f616acc5c98d8628914ba5acf5d04cdde5f1810
ar0305 <ar0305@kit.edu>
2025-05-13 17:09:13 +0200

    remove observer registration from usecase: CountPlotsUpdater

--- OTAnalytics/application/use_cases/update_count_plots.py
+++ OTAnalytics/application/use_cases/update_count_plots.py
@@ -1,5 +1,4 @@
 from application.state import TrackViewState
-from domain.event import EventRepository
 from plugin_ui.visualization.counts.counts_plotter import (
     CountsPlotter,
     MultipleCountsPlotters,
@@ -11,8 +10,6 @@ class CountPlotsUpdater:
     def __init__(self, state: TrackViewState, *plotters: CountsPlotter) -> None:
         self._state = state
 
-        state.view_width.register(lambda _: self.update())
-
         if len(plotters) > 1:
             self._plotter = MultipleCountsPlotters(list(plotters))
         elif len(plotters) == 1:
@@ -22,9 +19,6 @@ class CountPlotsUpdater:
 
     def __call__(self) -> None:
         self.update()
-
-    def register_at(self, event_repository: EventRepository) -> None:
-        event_repository.register_observer(lambda _: self.update())
 
     def update(self) -> None:
         width = self._state.view_width.get()
