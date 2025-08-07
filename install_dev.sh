88524cce7d2138f9e5dee32187a81243f570959c
Lars Briem <lars.briem@platomo.de>
2025-08-07 15:26:27 +0200

    Use uv in dev setup

--- install_dev.sh
+++ install_dev.sh
@@ -5,9 +5,8 @@ echo "Install OTAnalytics development environment."
 WORKING_DIR=$(pwd)
 VENV="$WORKING_DIR"/.venv
 PRE_COMMIT="$VENV"/bin/pre-commit
-UV="$VENV"/bin/uv
 
 bash "$WORKING_DIR"/install.sh
 
-$UV pip install -e .[dev] --python .venv
+uv sync --extra dev
 $PRE_COMMIT install --install-hooks
