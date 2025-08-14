c83cfbfea81f5b708dd936b1984012b00bd14f51
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2025-08-14 18:07:31 +0200

    Refactor dependency management to use `dev-dependencies` and adapt `uv` sync commands

--- install_dev.cmd
+++ install_dev.cmd
@@ -1,6 +1,7 @@
 echo Install OTAnalytics development environment.
 call install.cmd
 call .venv\Scripts\activate
-uv pip install -e .[dev] --python .venv
+uv pip install -e --python .venv
+uv sync --dev
 pre-commit install --install-hooks
 deactivate
