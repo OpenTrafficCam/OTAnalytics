f025d6f6adbfe928ebf3aa5aeeaa5078e2386594
Lars Briem <lars.briem@platomo.de>
2025-08-20 15:50:11 +0200

    Simplify shown video

--- OTAnalytics/application/state.py
+++ OTAnalytics/application/state.py
@@ -359,8 +359,9 @@ class SelectedVideoUpdate(TrackListObserver, VideoListObserver):
     def notify_tracks(self, track_event: TrackRepositoryEvent) -> None:
         all_tracks = self._datastore.get_all_tracks()
         if track_event.added:
-            first_track = next(iter(all_tracks))
-            if video := self._datastore.get_video_by_date(first_track.start):
+            if (track_start := all_tracks.first_occurrence) and (
+                video := self._datastore.get_video_by_date(track_start)
+            ):
                 self._track_view_state.selected_videos.set(video)
 
     def notify_videos(self, videos: list[Video]) -> None:
