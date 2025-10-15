fed50c63fb122bff91289ef5bfda597989c93b95
Lars Briem <lars.briem@platomo.de>
2025-10-15 14:46:45 +0200

    Compare csv files using polars and differ between events and counts

--- tests/regression_otanalytics.py
+++ tests/regression_otanalytics.py
@@ -8,7 +8,7 @@ from OTAnalytics.application.parser.cli_parser import CliMode
 from OTAnalytics.application.parser.flow_parser import FlowParser
 from OTAnalytics.plugin_cli.cli_application import OtAnalyticsCliApplicationStarter
 from OTAnalytics.plugin_parser.otvision_parser import OtFlowParser
-from tests.utils.assertions import assert_two_files_equal_sorted
+from tests.utils.assertions import assert_equal_count_files, assert_equal_event_files
 from tests.utils.builders.run_configuration import create_run_config
 
 
@@ -274,7 +274,7 @@ class TestRegressionCompleteApplication:
         expected_events_file = Path(test_data_dir / save_name).with_suffix(
             ".events.csv"
         )
-        assert_two_files_equal_sorted(actual_events_file, expected_events_file)
+        assert_equal_event_files(actual_events_file, expected_events_file)
 
         actual_counts_file = (
             Path(test_data_tmp_dir / save_name)
@@ -286,7 +286,7 @@ class TestRegressionCompleteApplication:
             .with_suffix(f".counts_{count_interval}min.csv")
             .absolute()
         )
-        assert_two_files_equal_sorted(actual_counts_file, expected_counts_file)
+        assert_equal_count_files(actual_counts_file, expected_counts_file)
 
     def _run_otanalytics(
         self,
