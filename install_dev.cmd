9cc7d23c035439afd5a766ba50afa38ae2e6ce99
Lars Briem <lars.briem@platomo.de>
2025-11-04 14:50:14 +0100

    OP#8345: Install playwright in dev environment

--- install_dev.cmd
+++ install_dev.cmd
@@ -2,5 +2,6 @@ echo Install OTAnalytics development environment.
 call install.cmd
 call .venv\Scripts\activate
 uv sync --dev --python .venv
+uv run playwright install
 pre-commit install --install-hooks
 deactivate
