ab924be42e7a616cd28f20fdf11452f5a2158cef
Lars Briem <lars.briem@platomo.de>
2025-12-15 10:38:30 +0100

    OP#8923: Refactor test code

--- tests/OTAnalytics/application/analysis/test_road_user_assignment.py
+++ tests/OTAnalytics/application/analysis/test_road_user_assignment.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from unittest.mock import Mock
 
 from OTAnalytics.application.analysis.road_user_assignment import (
@@ -15,24 +16,42 @@ class TestRoadUserAssignmentRepository:
         """
         Fix bug OP#8926 - dictionary changed size during iteration
         """
-        start_event = Mock()
-        start_event.road_user_id = "road_user_1"
-        end_event = Mock()
-        end_event.road_user_id = "road_user_1"
-        assignment = RoadUserAssignment(
-            road_user="road_user_1",
-            road_user_type="car",
-            assignment=Flow(
-                id=FlowId("flow_1"),
-                name="Flow 1",
-                start=SectionId("0"),
-                end=SectionId("1"),
-            ),
-            events=EventPair(start=start_event, end=end_event),
-        )
-        repository = RoadUserAssignmentRepository(track_id_set_factory=Mock())
-        repository.add(assignment)
+        given = setup()
 
-        repository.remove_assignments_of_event(start_event)
+        given.repository.remove_assignments_of_event(given.start_event)
 
-        assert repository.get_all().as_list() == []
+        assert given.repository.get_all().as_list() == []
+
+
+@dataclass
+class Given:
+    start_event: Mock
+    end_event: Mock
+    assignment: RoadUserAssignment
+    repository: RoadUserAssignmentRepository
+
+
+def setup() -> Given:
+    start_event = Mock()
+    start_event.road_user_id = "road_user_1"
+    end_event = Mock()
+    end_event.road_user_id = "road_user_1"
+    assignment = RoadUserAssignment(
+        road_user="road_user_1",
+        road_user_type="car",
+        assignment=Flow(
+            id=FlowId("flow_1"),
+            name="Flow 1",
+            start=SectionId("0"),
+            end=SectionId("1"),
+        ),
+        events=EventPair(start=start_event, end=end_event),
+    )
+    repository = RoadUserAssignmentRepository(track_id_set_factory=Mock())
+    repository.add(assignment)
+    return Given(
+        start_event=start_event,
+        end_event=end_event,
+        assignment=assignment,
+        repository=repository,
+    )
