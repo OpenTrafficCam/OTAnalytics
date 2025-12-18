9619e8778777e2684ddb88415d3e03586a2e95a8
Lars Briem <lars.briem@platomo.de>
2025-12-18 14:08:56 +0100

    OP#8949: Refactor test

--- tests/OTAnalytics/application/use_cases/test_get_road_user_assignments.py
+++ tests/OTAnalytics/application/use_cases/test_get_road_user_assignments.py
@@ -1,6 +1,5 @@
+from dataclasses import dataclass
 from unittest.mock import Mock, call
-
-import pytest
 
 from OTAnalytics.application.analysis.road_user_assignment import (
     RoadUserAssignmentRepository,
@@ -17,68 +16,81 @@ from OTAnalytics.application.use_cases.flow_repository import GetAllFlows
 from OTAnalytics.domain.track_dataset.track_dataset import TrackIdSetFactory
 from OTAnalytics.domain.types import EventType
 
-events = [Mock(), Mock()]
-flows = [Mock(), Mock()]
-assignments_as_list = [Mock(), Mock()]
-
-
-@pytest.fixture
-def flow_repository() -> Mock:
-    repository = Mock()
-    repository.get_all.return_value = flows
-    return repository
-
-
-@pytest.fixture
-def event_repository() -> Mock:
-    repository = Mock()
-    repository.get_all.return_value = events
-    repository.get.return_value = events
-    return repository
-
-
-@pytest.fixture
-def assignments() -> Mock:
-    assignments = Mock()
-    assignments.as_list.return_value = assignments_as_list
-    return assignments
-
-
-@pytest.fixture
-def road_user_assigner(assignments: Mock) -> Mock:
-    assigner = Mock()
-    assigner.assign.return_value = assignments
-    return assigner
-
 
 class TestGetRoadUserAssignments:
-    def test_get(
-        self,
-        flow_repository: Mock,
-        event_repository: Mock,
-        road_user_assigner: Mock,
-        assignments: Mock,
-    ) -> None:
-        mock_factory = Mock(spec=TrackIdSetFactory)
-        rua_repo = RoadUserAssignmentRepository(mock_factory)
-        create_events = Mock(spec=CreateEvents)
+    def test_get(self) -> None:
+        given = setup()
+        target = create_target(given)
 
-        create_assignments = CreateRoadUserAssignments(
-            GetAllFlows(flow_repository),
-            GetAllEnterSectionEvents(event_repository),
-            create_events,
-            road_user_assigner,
-            rua_repo,
-        )
+        actual = target.get_as_list()
 
-        get_assignments = GetRoadUserAssignments(rua_repo, create_assignments, True)
-        actual = get_assignments.get_as_list()
-        assert actual == assignments_as_list
-
+        assert actual == given.assignments_as_list
         args = call(event_types=[EventType.SECTION_ENTER])
-        event_repository.get.assert_has_calls([args])
-        event_repository.get_all.assert_not_called()
+        given.event_repository.get.assert_has_calls([args])
+        given.event_repository.get_all.assert_not_called()
+        given.flow_repository.get_all.assert_called_once()
+        given.road_user_assigner.assign.assert_called_once_with(
+            given.events, given.flows
+        )
+        given.assignments.as_list.assert_called_once()
 
-        flow_repository.get_all.assert_called_once()
-        road_user_assigner.assign.assert_called_once_with(events, flows)
-        assignments.as_list.assert_called_once()
+
+@dataclass
+class Given:
+    events: list[Mock]
+    flows: list[Mock]
+    assignments: Mock
+    assignments_as_list: list[Mock]
+    flow_repository: Mock
+    event_repository: Mock
+    road_user_assigner: Mock
+    create_events: Mock
+    assignment_repository: RoadUserAssignmentRepository
+
+
+def setup() -> Given:
+    events = [Mock(), Mock()]
+    flows = [Mock(), Mock()]
+    assignments_as_list = [Mock(), Mock()]
+
+    flow_repository = Mock()
+    flow_repository.get_all.return_value = flows
+
+    event_repository = Mock()
+    event_repository.get_all.return_value = events
+    event_repository.get.return_value = events
+
+    assignments = Mock()
+    assignments.as_list.return_value = assignments_as_list
+
+    road_user_assigner = Mock()
+    road_user_assigner.assign.return_value = assignments
+
+    create_events = Mock(spec=CreateEvents)
+
+    mock_factory = Mock(spec=TrackIdSetFactory)
+    assignment_repository = RoadUserAssignmentRepository(mock_factory)
+
+    return Given(
+        events=events,
+        flows=flows,
+        assignments=assignments,
+        assignments_as_list=assignments_as_list,
+        flow_repository=flow_repository,
+        event_repository=event_repository,
+        road_user_assigner=road_user_assigner,
+        create_events=create_events,
+        assignment_repository=assignment_repository,
+    )
+
+
+def create_target(given: Given) -> GetRoadUserAssignments:
+    create_assignments = CreateRoadUserAssignments(
+        GetAllFlows(given.flow_repository),
+        GetAllEnterSectionEvents(given.event_repository),
+        given.create_events,
+        given.road_user_assigner,
+        given.assignment_repository,
+    )
+
+    return GetRoadUserAssignments(given.assignment_repository, create_assignments, True)
