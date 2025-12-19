774548211e0efbf9923c3d875a7c85d9bea40f83
Lars Briem <lars.briem@platomo.de>
2025-12-19 10:35:13 +0100

    OP#8470: Refactor test cases

--- tests/OTAnalytics/application/use_cases/test_load_otconfig.py
+++ tests/OTAnalytics/application/use_cases/test_load_otconfig.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from datetime import datetime
 from unittest.mock import MagicMock, Mock
 
@@ -15,97 +16,149 @@ REMARK = "my remark"
 
 
 class TestLoadOtconfig:
-    @pytest.fixture
-    def otconfig(self) -> OtConfig:
-        project = Mock()
-        project.name = "my project"
-        project.start_date = datetime(2021, 1, 1)
-
-        analysis = Mock()
-        analysis.track_files = {"path/to/first.ottrk", "path/to/second.ottrk"}
-
-        otconfig = Mock()
-        otconfig.project = project
-        otconfig.videos = Mock()
-        otconfig.sections = Mock()
-        otconfig.flows = Mock()
-        otconfig.analysis = analysis
-        otconfig.remark = REMARK
-        return otconfig
-
-    def test_load(self, otconfig: OtConfig) -> None:
-        clear_repositories = Mock()
-        config_parser = Mock()
-        config_parser.parse.return_value = otconfig
-        remark_repository = Mock()
-        update_project = Mock()
-        add_videos = Mock()
-        add_sections = Mock()
-        add_flows = Mock()
-        load_track_files = Mock()
-        deserialization_result = Mock()
-        deserializer = Mock()
-        deserializer.return_value = deserialization_result
-
-        load_otconfig = LoadOtconfig(
-            clear_repositories,
-            config_parser,
-            update_project,
-            add_videos,
-            add_sections,
-            add_flows,
-            load_track_files,
-            remark_repository,
-            deserializer,
+    def test_load(self) -> None:
+        given = setup(
+            project_name="my project",
+            start_date=datetime(2021, 1, 1),
+            track_files={"path/to/first.ottrk", "path/to/second.ottrk"},
+            remark=REMARK,
+            raise_error=False,
         )
+        target = create_target(given)
         observer = Mock()
-        load_otconfig.register(observer)
+        target.register(observer)
 
         file = Mock()
-        load_otconfig.load(file)
+        target.load(file)
 
-        update_project.assert_called_once_with(
-            otconfig.project.name,
-            otconfig.project.start_date,
-            otconfig.project.metadata,
+        given.update_project.assert_called_once_with(
+            given.otconfig.project.name,
+            given.otconfig.project.start_date,
+            given.otconfig.project.metadata,
         )
-        add_videos.add.assert_called_once_with(otconfig.videos)
-        add_sections.add.assert_called_once_with(otconfig.sections)
-        add_flows.add.assert_called_once_with(otconfig.flows)
-        load_track_files.assert_called_once_with(list(otconfig.analysis.track_files))
+        given.add_videos.add.assert_called_once_with(given.otconfig.videos)
+        given.add_sections.add.assert_called_once_with(given.otconfig.sections)
+        given.add_flows.add.assert_called_once_with(given.otconfig.flows)
+        given.load_track_files.assert_called_once_with(
+            list(given.otconfig.analysis.track_files)
+        )
         observer.assert_called_once_with(
-            ConfigurationFile(file, deserialization_result)
+            ConfigurationFile(file, given.deserialization_result)
         )
-        clear_repositories.assert_called_once()
-        remark_repository.add.assert_called_once_with(REMARK)
+        given.clear_repositories.assert_called_once()
+        given.remark_repository.add.assert_called_once_with(REMARK)
 
-    def test_load_error(self, otconfig: OtConfig) -> None:
-        clear_repositories = Mock()
-        config_parser = Mock()
-        config_parser.parse.return_value = otconfig
+    def test_load_error(self) -> None:
+        given = setup(
+            project_name="my project",
+            start_date=datetime(2021, 1, 1),
+            track_files={"path/to/first.ottrk", "path/to/second.ottrk"},
+            remark=REMARK,
+            raise_error=True,
+        )
+        target = create_target(given)
+        observer = Mock()
+        target.register(observer)
+        file = Mock()
 
+        with pytest.raises(UnableToLoadOtconfigFile):
+            target.load(file)
+
+        assert given.clear_repositories.call_count == 2
+        given.config_parser.parse.assert_called_once_with(file)
+        observer.assert_not_called()
+        given.remark_repository.add.assert_not_called()
+
+
+@dataclass
+class Given:
+    otconfig: OtConfig
+    clear_repositories: Mock
+    config_parser: Mock
+    update_project: Mock
+    add_videos: Mock
+    add_sections: Mock
+    add_flows: Mock
+    load_track_files: Mock
+    remark_repository: Mock
+    deserializer: Mock
+    deserialization_result: Mock
+
+
+def setup(
+    project_name: str,
+    start_date: datetime,
+    track_files: set[str],
+    remark: str,
+    raise_error: bool,
+) -> Given:
+    otconfig = create_otconfig(project_name, start_date, track_files, remark)
+
+    clear_repositories = Mock()
+    config_parser = Mock()
+    config_parser.parse.return_value = otconfig
+    update_project = Mock()
+    add_videos = Mock()
+    add_flows = Mock()
+    load_track_files = Mock()
+    remark_repository = Mock()
+    deserialization_result = Mock()
+    deserializer = Mock()
+    deserializer.return_value = deserialization_result
+
+    if raise_error:
         add_sections = MagicMock()
         add_sections.add.side_effect = SectionAlreadyExists
-        remark_repository = Mock()
+    else:
+        add_sections = Mock()
 
-        load_otconfig = LoadOtconfig(
-            clear_repositories,
-            config_parser,
-            Mock(),
-            Mock(),
-            add_sections,
-            Mock(),
-            Mock(),
-            remark_repository,
-            Mock(),
-        )
-        observer = Mock()
-        load_otconfig.register(observer)
-        file = Mock()
-        with pytest.raises(UnableToLoadOtconfigFile):
-            load_otconfig.load(file)
+    return Given(
+        otconfig=otconfig,
+        clear_repositories=clear_repositories,
+        config_parser=config_parser,
+        update_project=update_project,
+        add_videos=add_videos,
+        add_sections=add_sections,
+        add_flows=add_flows,
+        load_track_files=load_track_files,
+        remark_repository=remark_repository,
+        deserializer=deserializer,
+        deserialization_result=deserialization_result,
+    )
 
-        assert clear_repositories.call_count == 2
-        config_parser.parse.assert_called_once_with(file)
-        observer.assert_not_called()
-        remark_repository.add.assert_not_called()
+
+def create_otconfig(
+    project_name: str,
+    start_date: datetime,
+    track_files: set[str],
+    remark: str,
+) -> OtConfig:
+    project = Mock()
+    project.name = project_name
+    project.start_date = start_date
+
+    analysis = Mock()
+    analysis.track_files = track_files
+
+    otconfig = Mock()
+    otconfig.project = project
+    otconfig.videos = Mock()
+    otconfig.sections = Mock()
+    otconfig.flows = Mock()
+    otconfig.analysis = analysis
+    otconfig.remark = remark
+    return otconfig
+
+
+def create_target(given: Given) -> LoadOtconfig:
+    return LoadOtconfig(
+        given.clear_repositories,
+        given.config_parser,
+        given.update_project,
+        given.add_videos,
+        given.add_sections,
+        given.add_flows,
+        given.load_track_files,
+        given.remark_repository,
+        given.deserializer,
+    )
