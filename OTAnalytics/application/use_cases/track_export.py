b96a01ee53c9d6a98ec1d3049ba7e4d50ca0b1f1
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2026-05-08 21:36:43 +0200

    OP#8548: Remove unused MultiExportTracks

--- OTAnalytics/application/use_cases/track_export.py
+++ OTAnalytics/application/use_cases/track_export.py
@@ -29,30 +29,3 @@ class ExportTracks(ABC):
         specification: TrackExportSpecification,
     ) -> None:
         raise NotImplementedError
-
-
-class MultiExportTracks(ExportTracks):
-    """
-    Class representing a multi-format track exporter.
-    """
-
-    def __init__(self, exporter: dict[TrackFileFormat, ExportTracks]) -> None:
-        """
-        Initialize the MultiExportTracks object with a dictionary of ExportTracks
-        objects for different formats.
-
-        Args: exporter (dict[TrackFileFormat, ExportTracks]): A dictionary containing
-            ExportTracks objects for each format.
-        """
-        self._exporter = exporter
-
-    def export(self, specification: TrackExportSpecification) -> None:
-        """
-        Exports tracks using the specified format.
-
-        Args: specification (TrackExportSpecification): The export specification
-            containing the format and other necessary details.
-        """
-        for export_format in specification.export_format:
-            if export_format in self._exporter.keys():
-                self._exporter[export_format].export(specification)
